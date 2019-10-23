ea9008dc0bdfb9afafc1ec90e3913fb5b774b5f5
blAs1N <qazasa123@gmail.com>
2019-10-23 11:18:08 +0900

    Clean comment

--- BS-Engine/Source/Memory/Memory/Public/IMemory.h
+++ BS-Engine/Source/Memory/Memory/Public/IMemory.h
@@ -8,6 +8,12 @@
  * Use Malloc / Free instead of new / delete.
 */
 INTERFACE_BEGIN(Memory)
+	/// @brief Allocate new memory.
 	INTERFACE_DEF(void*, Malloc, size_t);
+
+	/// @brief Free all memory up to the argument.
 	INTERFACE_DEF(void, Free, void*);
+
+	/// @brief Clear memory
+	INTERFACE_DEF(void, Clear);
 INTERFACE_END
--- BS-Engine/Source/Memory/Memory/Public/StackMemory.h
+++ BS-Engine/Source/Memory/Memory/Public/StackMemory.h
@@ -3,15 +3,17 @@
 #include "IMemory.h"
 #include <limits>
 
-/// @brief Memory with fixed order of allocation and deallocation
+/**
+ * @brief Memory with fixed order of allocation and deallocation
+ * @ref IMemory.h
+*/
 class BS_API StackMemory final : public IMemory
 {
 public:
-	StackMemory(size_t size) noexcept;
+	constexpr StackMemory(size_t size) noexcept;
 	~StackMemory();
 
 	/**
-	 * @brief Allocate new memory.
 	 * @param[in] size How much memory to allocate.
 	 * @return Address of allocated memory.
 	 * @retval nullptr Returned when the memory is full and can no longer be allocated.
@@ -19,14 +21,12 @@ public:
 	void* Malloc(size_t size) override;
 
 	/**
-	 * @brief Free all memory up to the argument.
 	 * @param[in] ptr New top pointer
 	 * @warning If you try to free a pointer that is not at the top, all the pointers above it are freed.
 	*/
 	void Free(void* ptr) override;
 
-	/// @brief Clear memory
-	void Clear();
+	void Clear() override;
 
 private:
 	using byte = unsigned char;

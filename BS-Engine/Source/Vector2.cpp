5eb18d9e486e1d74b4b3a849fe66e5516c4fee1b
blAs1N <qazasa123@gmail.com>
2019-10-25 00:00:50 +0900

    Implement const value

---
+++ BS-Engine/Source/Vector2.cpp
@@ -0,0 +1,6 @@
+#include "Vector2.h"
+
+const Vector2 Zero{ 0.0f, 0.0f };
+const Vector2 One{ 1.0f, 1.0f };
+const Vector2 UnitX{ 1.0f, 0.0f };
+const Vector2 UnitY{ 0.0f, 1.0f };
---
+++ BS-Engine/Source/Vector4.cpp
@@ -0,0 +1,8 @@
+#include "Vector4.h"
+
+const Vector4 Zero{ 0.0f, 0.0f, 0.0f, 0.0f };
+const Vector4 One{ 1.0f, 1.0f, 1.0f, 1.0f };
+const Vector4 UnitX{ 1.0f, 0.0f, 0.0f, 0.0f };
+const Vector4 UnitY{ 0.0f, 1.0f, 0.0f, 0.0f };
+const Vector4 UnitZ{ 0.0f, 0.0f, 1.0f, 0.0f };
+const Vector4 UnitW{ 0.0f, 0.0f, 0.0f, 1.0f };

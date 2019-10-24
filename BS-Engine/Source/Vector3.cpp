8f998ef299f482b4d264d8937f2fb140543b2bf8
blAs1N <qazasa123@gmail.com>
2019-10-25 00:00:50 +0900

    Implement const value

---
+++ BS-Engine/Source/Vector3.cpp
@@ -0,0 +1,7 @@
+#include "Vector3.h"
+
+const Vector3 Zero{ 0.0f, 0.0f, 0.0f };
+const Vector3 One{ 1.0f, 1.0f, 1.0f };
+const Vector3 UnitX{ 1.0f, 0.0f, 0.0f };
+const Vector3 UnitY{ 0.0f, 1.0f, 0.0f };
+const Vector3 UnitZ{ 0.0f, 0.0f, 1.0f };

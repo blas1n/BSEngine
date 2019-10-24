9a7e172e4f0539e14fb4b2c50d10b88e9685fbe1
blAs1N <qazasa123@gmail.com>
2019-10-24 09:29:07 +0900

    Separated interface macro

--- BS-Engine/Include/Macro.h
+++ BS-Engine/Include/Macro.h
@@ -12,19 +12,8 @@ using tchar = char;
 #define TEXT(x) x
 #endif
 
-#define INTERFACE_BEGIN(name) \
-class BS_API I##name abstract { \
-	public:
-#define INTERFACE_END };
-
-#define INTERFACE_DEF(ret, name, ...) \
-virtual ret name(__VA_ARGS__) noexcept = 0;
-
-#define CONST_INTERFACE_DEF(ret, name, ...) \
-virtual ret name(__VA_ARGS__) const noexcept = 0;
-
 #if _DEBUG
-#define check(expr) assert(expr); 
+#define check(expr) { if (!(expr)) { assert(false); } }
 #else
-#define check(expr) { if (!expr) { return; } }
+#define check(expr) { if (!(expr)) { return; } }
 #endif

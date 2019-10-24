0ae6adf8824e7fbf378d08e771fbaecd48ce8be0
blAs1N <qazasa123@gmail.com>
2019-10-24 11:35:58 +0900

    More pool memory test

--- Test/PoolMemoryTest.cpp
+++ Test/PoolMemoryTest.cpp
@@ -4,6 +4,8 @@
 TEST(PoolMemory, Create)
 {
 	PoolMemory<1> pool{ 10 };
+	ASSERT_EQ(pool.GetAssignedByte(), 0);
+	ASSERT_EQ(pool.GetAssignableByte(), 10);
 	ASSERT_EQ(pool.GetMaxByte(), 10);
 }
 
@@ -14,7 +16,7 @@ TEST(PoolMemory, Malloc)
 	ASSERT_EQ(p, nullptr);
 
 	p = pool.Malloc(3);
-	ASSERT_TRUE(p != nullptr);
+	ASSERT_EQ(pool.GetAssignableByte(), 2 * sizeof(int));
 
 	p = pool.Malloc(3);
 	ASSERT_EQ(p, nullptr);
@@ -26,15 +28,31 @@ TEST(PoolMemory, Malloc)
 TEST(PoolMemory, Free)
 {
 	PoolMemory<sizeof(int)> pool{ 5 };
-	ASSERT_DEATH(pool.Free(nullptr), "Assertion failed");
 
-	for (auto i = 5; i > 0; --i)
-	{
-		auto p = pool.Malloc(i);
-		pool.Free(p);
-		ASSERT_EQ(pool.GetAssignedByte(), 0);
-	}
+	auto p = pool.Malloc(5);
+	pool.Free(p);
+	ASSERT_EQ(pool.GetAssignedByte(), 0);
 
-	const auto p = static_cast<unsigned char*>(pool.Malloc(1)) - 1;
-	ASSERT_DEATH(pool.Free(p), "Assertion failed");
+	auto a = pool.Malloc(2);
+	auto b = pool.Malloc(1);
+	auto c = pool.Malloc(2);
+
+	pool.Free(b);
+	ASSERT_EQ(pool.GetAssignableByte(), sizeof(int));
+
+	pool.Free(c);
+	ASSERT_EQ(pool.GetAssignedByte(), 2 * sizeof(int));
+
+	pool.Free(a);
+	ASSERT_EQ(pool.GetAssignedByte(), 0);	
+}
+
+TEST(PoolMemory, FreeInvalidArgument)
+{
+	PoolMemory<sizeof(int)> pool{ 5 };
+
+	auto p = static_cast<unsigned char*>(pool.Malloc(1));
+	ASSERT_DEBUG_DEATH(pool.Free(p - 1), ".*");
+	ASSERT_DEBUG_DEATH(pool.Free(p + 1), ".*");
+	ASSERT_DEBUG_DEATH(pool.Free(nullptr), ".*");
 }

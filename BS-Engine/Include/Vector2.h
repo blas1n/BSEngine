edfc8461e43efe2ecd1213996efdd05bed6967c8
blAs1N <qazasa123@gmail.com>
2019-10-24 23:14:55 +0900

    Add noexcept keyword

--- BS-Engine/Include/Vector2.h
+++ BS-Engine/Include/Vector2.h
@@ -14,29 +14,29 @@ public:
 	void Set(float inX, float inY) noexcept;
 
 	Vector2 operator-() const noexcept;
-	Vector2& operator+=(const Vector2& other);
-	Vector2& operator-=(const Vector2& other);
-	Vector2 operator*=(const Vector2& other);
-	Vector2& operator*=(const float scalar);
-	Vector2 operator/=(const Vector2& other);
-	Vector2& operator/=(const float scalar);
+	Vector2& operator+=(const Vector2& other) noexcept;
+	Vector2& operator-=(const Vector2& other) noexcept;
+	Vector2 operator*=(const Vector2& other) noexcept;
+	Vector2& operator*=(const float scalar) noexcept;
+	Vector2 operator/=(const Vector2& other) noexcept;
+	Vector2& operator/=(const float scalar) noexcept;
 
-	float LengthSquared() const;
-	float Length() const;
+	float LengthSquared() const noexcept;
+	float Length() const noexcept;
 
-	void Normalized();
-	static Vector2 Normalize(const Vector2& vec);
+	void Normalized() noexcept;
+	static Vector2 Normalize(const Vector2& vec) noexcept;
 
-	static float Dot(const Vector2& lhs, const Vector2& rhs);
-	static Vector2 Reflect(const Vector2& v, const Vector2& n);
-	static Vector2 Transform(const Vector2& vec, const class Matrix3& mat, float w = 1.0f);
+	static float Dot(const Vector2& lhs, const Vector2& rhs) noexcept;
+	static Vector2 Reflect(const Vector2& v, const Vector2& n) noexcept;
+	static Vector2 Transform(const Vector2& vec, const class Matrix3& mat, float w = 1.0f) noexcept;
 
 	// freinds
-	friend Vector2 operator+(const Vector2& lhs, const Vector2& rhs);
-	friend Vector2 operator-(const Vector2& lhs, const Vector2& rhs);
-	friend Vector2 operator*(const Vector2& lhs, const Vector2& rhs);
-	friend Vector2 operator*(const Vector2& vec, const float scalar);
-	friend Vector2 operator*(const float scalar, const Vector2& vec);
-	friend Vector2 operator/(const Vector2& lhs, const Vector2& rhs);
-	friend Vector2 operator/(const Vector2& vec, const float scalar);
+	friend Vector2 operator+(const Vector2& lhs, const Vector2& rhs) noexcept;
+	friend Vector2 operator-(const Vector2& lhs, const Vector2& rhs) noexcept;
+	friend Vector2 operator*(const Vector2& lhs, const Vector2& rhs) noexcept;
+	friend Vector2 operator*(const Vector2& vec, const float scalar) noexcept;
+	friend Vector2 operator*(const float scalar, const Vector2& vec) noexcept;
+	friend Vector2 operator/(const Vector2& lhs, const Vector2& rhs) noexcept;
+	friend Vector2 operator/(const Vector2& vec, const float scalar) noexcept;
 };

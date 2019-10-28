70791c60d5ccede518bf05ad8eeeb44668fe6047
blAs1N <qazasa123@gmail.com>
2019-10-28 23:42:33 +0900

    Add to interface macros

--- BS-Engine/Include/Interface.h
+++ BS-Engine/Include/Interface.h
@@ -5,10 +5,14 @@
 #define INTERFACE_BEGIN(name) \
 class BS_API I##name abstract { \
 	public:
+
 #define INTERFACE_END };
 
 #define INTERFACE_DEF(ret, name, ...) \
 virtual ret name(__VA_ARGS__) noexcept = 0;
 
-#define CONST_INTERFACE_DEF(ret, name, ...) \
+#define INTERFACE_CONST_DEF(ret, name, ...) \
 virtual ret name(__VA_ARGS__) const noexcept = 0;
+
+#define INTERFACE_TYPE_DEF(name, type) \
+using name = type;

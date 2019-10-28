6e9c0adce760ce4e61498a1e8f8cb3dbf9a0c890
blAs1N <qazasa123@gmail.com>
2019-10-28 22:29:20 +0900

    Move folder

--- BS-Engine/IDescriptor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Interface.h"
-
-INTERFACE_BEGIN(Descriptor)
-	CONST_INTERFACE_DEF(const char*, GetName)
-	CONST_INTERFACE_DEF(void*, GetParamsAddress)
-	CONST_INTERFACE_DEF(size_t, GetParamsSize)
-INTERFACE_END

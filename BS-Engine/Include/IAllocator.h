b5c568daf6885ac24416ee294d7763109a0569e1
blAs1N <qazasa123@gmail.com>
2019-10-28 23:42:18 +0900

    Change allocator interface

--- BS-Engine/Include/IAllocator.h
+++ BS-Engine/Include/IAllocator.h
@@ -11,6 +11,16 @@
 */
 template <class T>
 INTERFACE_BEGIN(Allocator)
+	INTERFACE_TYPE_DEF(value_type, T)
+
 	INTERFACE_DEF(T*, allocate, size_t)
 	INTERFACE_DEF(void, deallocate, T*, size_t)
+
+	template <class U, class... Args>
+	INTERFACE_DEF(void, construct, U*, Args&&...)
+
+	template <class U>
+	INTERFACE_DEF(void, destroy, U*);
+
+	INTERFACE_CONST_DEF(size_t, max_size);
 INTERFACE_END

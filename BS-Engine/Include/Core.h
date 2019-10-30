07685c2b4b2bab2f6fc99220f6cfd4fc10e1b5cc
blAs1N <qazasa123@gmail.com>
2019-10-30 15:08:38 +0900

    Type Separation from Core Headers

--- BS-Engine/Include/Core.h
+++ BS-Engine/Include/Core.h
@@ -1,13 +1,4 @@
 #pragma once
 
 #include "Interface.h"
-
-using uint8 = unsigned char;
-using uint16 = unsigned short;
-using uint32 = unsigned int;
-using uint64 = unsigned long long;
-
-using int8 = signed int;
-using int16 = signed short;
-using int32 = signed int;
-using int64 = signed long long;
+#include "Type.h"

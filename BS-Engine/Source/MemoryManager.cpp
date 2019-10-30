d2d4ad75d684eff27dcad47d78b79ea124bda9ee
blAs1N <qazasa123@gmail.com>
2019-10-30 23:56:09 +0900

    Build memory manager

--- BS-Engine/Source/MemoryManager.cpp
+++ BS-Engine/Source/MemoryManager.cpp
@@ -1,10 +1,15 @@
 #include "MemoryManager.h"
 #include "HeapMemory.h"
 
+constexpr size_t MEMORY_SIZE = 30000;
+
+MemoryManager::MemoryManager() noexcept
+	: memory(nullptr) {}
+
 bool MemoryManager::Init() noexcept
 {
 	memory = new HeapMemory{ };
-	return memory->Init();
+	return memory->Init(MEMORY_SIZE);
 }
 
 void MemoryManager::Update(const float deltaTime) noexcept

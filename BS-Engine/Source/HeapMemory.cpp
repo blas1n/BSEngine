3239423ce85a410df53e62d65acd0a5a1cfd9a40
blAs1N <qazasa123@gmail.com>
2019-11-03 01:05:20 +0900

    Merge heap memory and marker memory

--- BS-Engine/Source/HeapMemory.cpp
+++ BS-Engine/Source/HeapMemory.cpp
@@ -1,40 +1,75 @@
 #include "HeapMemory.h"
-#include "MarkerMemory.h"
 #include "MathFunctions.h"
+#include <memory>
+
+inline bool IsAllocated(const uint8* const marker, const size_t index) noexcept
+{
+	return (marker[index / 8] & (1 << (index % 8))) > 0;
+}
+
+inline void Mark(uint8* const marker, const size_t index) noexcept
+{
+	marker[index / 8] |= 1 << (index % 8);
+}
+
+inline void Unmark(uint8* const marker, const size_t index) noexcept
+{
+	marker[index / 8] &= ~(1 << (index % 8));
+}
 
 bool HeapMemory::Init(const size_t size) noexcept
 {
-	const auto markerSize = static_cast<size_t>(
-		Math::Ceil(static_cast<float>(size) * 0.125f));
+	if (size == 0) return false;
 
-	auto* const memory = static_cast<uint8*>(std::malloc(size + markerSize + sizeof(MarkerMemory)));
+	maxNum = size;
+	markerSize = static_cast<size_t>(
+		Math::Ceil(static_cast<float>(maxNum) * 0.125f));
+
+	memory = static_cast<uint8*>(std::malloc(maxNum + markerSize));
 	if (memory == nullptr) return false;
 
-	auto* const marker = memory + size;
-	
-#pragma push_macro("new")
-#undef new
-	auto* markerPtr = reinterpret_cast<MarkerMemory*>(marker + markerSize);
-	markerMemory = new(markerPtr)MarkerMemory{ memory, marker, size, markerSize };
-#pragma pop_macro("new")
-
+	marker = memory + maxNum;
+	std::memset(memory, 0, maxNum + markerSize);
 	return true;
 }
 
 void HeapMemory::Release() noexcept
 {
-	auto* const memory = markerMemory->GetMemory();
 	if (memory) std::free(memory);
-
-	markerMemory->~MarkerMemory();
 }
 
 void* HeapMemory::Malloc(const size_t n) noexcept
 {
-	return markerMemory->Alloc(n);
+	if (n == 0 || maxNum - curNum < n)
+		return nullptr;
+
+	for (size_t i = 0, clearSectionNum = 0; i < maxNum; ++i) {
+		if (IsAllocated(marker, i))
+		{
+			clearSectionNum = 0;
+			continue;
+		}
+
+		if (++clearSectionNum < n) continue;
+
+		const auto startIdx = i - n + 1;
+		for (auto idx = startIdx; idx <= i; ++idx)
+			Mark(marker, idx);
+
+		curNum += n;
+		std::memset(memory + startIdx, 0, n);
+		return memory + startIdx;
+	}
+
+	return nullptr;
 }
 
 void HeapMemory::Free(void* const ptr, const size_t n) noexcept
 {
-	markerMemory->Dealloc(ptr, n);
+	check(ptr >= memory && ptr < memory + maxNum);
+
+	curNum -= n;
+	const size_t startIdx = static_cast<uint8*>(ptr) - memory;
+	for (auto i = startIdx; i < n + startIdx; ++i)
+		Unmark(marker, i);
 }

c887b3d22adcf9f41225b7d23bf607355960ef5d
blAs1N <qazasa123@gmail.com>
2019-11-03 01:06:15 +0900

    Modify to match

--- BS-Engine/Include/MemoryManager.h
+++ BS-Engine/Include/MemoryManager.h
@@ -1,22 +1,21 @@
 #pragma once
 
 #include "IManager.h"
-#include "IAllocatorBase.h"
-#include "Array.h"
 
 class BS_API MemoryManager final : public IManager
 {
 public:
+	constexpr static size_t MEMORY_SIZE = 30000;
+
 	MemoryManager() noexcept;
 
 	bool Init() noexcept override;
-	void Update(float deltaTime) noexcept override;
+	void Update(float deltaTime) noexcept override {}
 	void Release() noexcept override;
 
-	void* Allocate(IAllocatorBase* alloc, size_t n) noexcept;
-	void Deallocate(IAllocatorBase* alloc, void* ptr, size_t n) noexcept;
+	void* Allocate(size_t n) noexcept;
+	void Deallocate(void* ptr, size_t n) noexcept;
 
 private:
 	class HeapMemory* memory;
-	Array<IAllocatorBase*>* singleFrameAllocators;
 };
--- BS-Engine/Source/MemoryManager.cpp
+++ BS-Engine/Source/MemoryManager.cpp
@@ -2,38 +2,15 @@
 #include "HeapMemory.h"
 #include <algorithm>
 
-constexpr size_t MEMORY_SIZE = 30000;
-constexpr size_t MAX_SINGLE_FRAME_ALLOCATOR = 100;
-
 MemoryManager::MemoryManager() noexcept
-	: memory(nullptr),
-	singleFrameAllocators(nullptr) {}
+	: memory(nullptr) {}
 
 bool MemoryManager::Init() noexcept
 {
 	memory = new HeapMemory{ };
 	if (!memory->Init(MEMORY_SIZE))
 		return false;
-
-	constexpr auto singleFrameSize = MAX_SINGLE_FRAME_ALLOCATOR * sizeof(IAllocatorBase*);
-
-	PoolAllocator<IAllocatorBase*> alloc{ MAX_SINGLE_FRAME_ALLOCATOR };
-
-	singleFrameAllocators =
-		static_cast<Array<IAllocatorBase*>*>(Allocate(&alloc, sizeof(Array<IAllocatorBase*>)));
-	
-#pragma push_macro("new")
-#undef new
-	singleFrameAllocators = new(singleFrameAllocators)Array<IAllocatorBase*>{ alloc };
-#pragma pop_macro("new")
-
-	return singleFrameAllocators != nullptr;
-}
-
-void MemoryManager::Update(const float deltaTime) noexcept
-{
-	for (const auto alloc : *singleFrameAllocators)
-		alloc->Clear();
+	return true;
 }
 
 void MemoryManager::Release() noexcept
@@ -42,24 +19,12 @@ void MemoryManager::Release() noexcept
 	delete memory;
 }
 
-void* MemoryManager::Allocate(IAllocatorBase* const alloc, const size_t n) noexcept
+void* MemoryManager::Allocate(const size_t n) noexcept
 {
-	if (alloc->IsSingleFrame())
-		singleFrameAllocators->emplace_back(alloc);
-
 	return memory->Malloc(n);
 }
 
-void MemoryManager::Deallocate(IAllocatorBase* const alloc, void* const ptr, const size_t n) noexcept
+void MemoryManager::Deallocate(void* const ptr, const size_t n) noexcept
 {
-	if (alloc->IsSingleFrame())
-	{
-		const auto iter = std::find(
-			singleFrameAllocators->cbegin(), singleFrameAllocators->cend(), alloc);
-
-		check(iter != singleFrameAllocators->cend());
-		singleFrameAllocators->erase(iter);
-	}
-
 	memory->Free(ptr, n);
 }

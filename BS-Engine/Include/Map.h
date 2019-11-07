414ef60b88a816061ac1c51cb6ab0627b5f7ebc1
blAs1N <qazasa123@gmail.com>
2019-11-07 20:06:42 +0900

    Fix wrong template argument

--- BS-Engine/Include/Map.h
+++ BS-Engine/Include/Map.h
@@ -4,8 +4,20 @@
 #include <unordered_map>
 #include "Allocator.h"
 
-template <class T, template<class>class Alloc = Allocator>
-using TreeMap = std::map<T, Alloc<T>>;
+/**
+ * @brief Templated map using tree.
+ * @detail Data is always sorted, but slightly slow.
+ * @todo Direct implementation.
+ * @warning Key type supports less or <, or requires custom comparator.
+*/
+template <class K, class T, template<class>class Comp = std::less>
+using TreeMap = std::map<K, T, Comp<K>, Allocator<std::pair<const K, T>>>;
 
-template <class T, template<class>class Alloc = Allocator>
-using HashMap = std::unordered_map<T, Alloc<T>>;
+/**
+ * @brief Templated map using hash.
+ * @detail Very fast, but the data is not sorted.
+ * @todo Direct implementation.
+ * @warning Key type supports std::hash. And supports std::equal_to or ==.
+*/
+template <class K, class T>
+using HashMap = std::unordered_map<K, T, std::hash<K>, std::equal_to<K>, Allocator<std::pair<const K, T>>>;
--- BS-Engine/Include/Set.h
+++ BS-Engine/Include/Set.h
@@ -4,8 +4,20 @@
 #include <unordered_set>
 #include "Allocator.h"
 
-template <class T, template<class>class Alloc = Allocator>
-using TreeSet = std::set<T, Alloc<T>>;
+/**
+ * @brief Templated set using tree.
+ * @detail Data is always sorted, but slightly slow.
+ * @todo Direct implementation.
+ * @warning Value type supports less or <, or requires custom comparator.
+*/
+template <class T, template<class>class Comp = std::less>
+using TreeSet = std::set<T, Comp<T>, Allocator<T>>;
 
-template <class T, template<class>class Alloc = Allocator>
-using HashSet = std::unordered_set<T, Alloc<T>>;
+/**
+ * @brief Templated set using hash.
+ * @detail Very fast, but the data is not sorted.
+ * @todo Direct implementation.
+ * @warning Key type supports std::hash. And supports std::equal_to or ==.
+*/
+template <class T, template<class>class Comp = std::less>
+using HashSet = std::unordered_set<T, std::hash<T>, std::equal_to<T>, Allocator<T>>;

76093bc8d570580d019777af75f2cc133367419b
blAs1N <qazasa123@gmail.com>
2019-11-07 18:59:33 +0900

    Delete Manager Manager

--- BS-Engine/Include/IManager.h
+++ BS-Engine/Include/IManager.h
@@ -3,22 +3,6 @@
 #include "Interface.h"
 #include "Type.h"
 
-/// @see This is good architecture?
-enum class EManager : uint8
-{
-	ManagerManager,
-	MemoryManager,
-	ThreadManager,
-	RenderingManager,
-	AudioManager,
-	InputManager,
-	PhysicsManager,
-	ResourceManager,
-	AnimationManager,
-	UiManager,
-	ConfigManager
-};
-
 INTERFACE_BEGIN(Manager)
 	INTERFACE_DEF(bool, Init)
 	INTERFACE_DEF(void, Update, float)

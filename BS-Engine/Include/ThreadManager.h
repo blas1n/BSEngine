bb1f02ae1a868314a041aa89d1bba2f10de20956
blAs1N <qazasa123@gmail.com>
2019-11-10 02:53:34 +0900

    Build thread manager

--- BS-Engine/Include/ThreadManager.h
+++ BS-Engine/Include/ThreadManager.h
@@ -1,50 +1,43 @@
 #pragma once
 
 #include "IManager.h"
-#include "Array.h"
-#include "Queue.h"
-#include <thread>
+#include <vector>
 #include <future>
-#include <functional>
+#include <queue>
 #include <mutex>
-#include <condition_variable>
-#include <cstdio>
 
-class BS_API ThreadManager final : public IManager
-{
+class ThreadManager : public IManager {
 public:
 	bool Init() noexcept override;
 	void Update(float deltaTime) noexcept override {}
 	void Release() noexcept override;
 
-	template <class Fn, class... Args> 
-	std::future<std::invoke_result_t<Fn, Args...>> AddTask(Fn&& fn, Args&&... args);
+	template <class Fn, class... Args>
+	std::future<std::invoke_result_t<Fn, Args...>> AddTask(
+		Fn&& fn, Args&&... args);
 
 private:
-	void Work() noexcept;
+	void ThreadWork();
 
 private:
-	Array<std::thread> threads;
-	Queue<std::function<void()>> tasks;
+	std::vector<std::thread> threads;
+	std::queue<std::function<void()>> tasks;
 	std::condition_variable cv;
-	std::mutex jobMutex;
-	bool isEnd = false;
+	std::mutex taskMutex;
+	bool isEnd;
 };
 
 template <class Fn, class... Args>
-std::future<std::invoke_result_t<Fn, Args...>> ThreadManager::AddTask(Fn&& fn, Args&&... args)
-{
+std::future<std::invoke_result_t<Fn, Args...>> ThreadManager::AddTask(Fn&& fn, Args&&... args) {
 	auto task = std::make_shared<
 		std::packaged_task<std::invoke_result_t<Fn, Args...>()>>(
 			std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...)
 		);
 
-	auto job_result_future = task->get_future();
-
-	jobMutex.lock();
+	taskMutex.lock();
 	tasks.push([task] { (*task)(); });
-	jobMutex.unlock();
-
+	taskMutex.unlock();
+	
 	cv.notify_one();
-	return job_result_future;
+	return task->get_future();
 }

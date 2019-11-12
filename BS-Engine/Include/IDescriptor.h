06579f9844c9fa78aeb6c60cf69beaa21ab69890
blAs1N <qazasa123@gmail.com>
2019-11-12 20:12:50 +0900

    Delete descriptor

--- BS-Engine/Include/IDescriptor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Interface.h"
-
-INTERFACE_BEGIN(Descriptor)
-	INTERFACE_CONST_DEF(const char*, GetName)
-	INTERFACE_CONST_DEF(void*, GetParamsAddress)
-	INTERFACE_CONST_DEF(size_t, GetParamsSize)
-INTERFACE_END

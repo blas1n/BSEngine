e1bf1281eefaa32dca36c1e1a425fea813ae33f4
blAs1N <qazasa123@gmail.com>
2019-11-15 23:14:21 +0900

    Delete abstract keyword because it is not standard keyword

--- engine/include/Interface.h
+++ engine/include/Interface.h
@@ -3,11 +3,11 @@
 #include "Macro.h"
 
 #define INTERFACE_BEGIN(name) \
-class BS_API I##name abstract { \
+class BS_API I##name { \
 public: \
 	virtual ~I##name() noexcept = default;
 
 #define INTERFACE_END };
 
-#define INTERFACE_DEF(ret, name, ...) virtual ret name(__VA_ARGS__) noexcept abstract = 0;
-#define INTERFACE_CONST_DEF(ret, name, ...) virtual ret name(__VA_ARGS__) const noexcept abstract = 0;
+#define INTERFACE_DEF(ret, name, ...) virtual ret name(__VA_ARGS__) noexcept = 0;
+#define INTERFACE_CONST_DEF(ret, name, ...) virtual ret name(__VA_ARGS__) const noexcept = 0;

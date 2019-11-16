575c3f7436874b06e0da6b68a2e7cccd1f423ea7
blAs1N <qazasa123@gmail.com>
2019-11-16 23:49:50 +0900

    Link memory manager and allocator
    Using Service Mediator Pattern

---
+++ engine/src/Allocator.cpp
@@ -0,0 +1,12 @@
+#include "Allocator.h"
+#include "MemoryManager.h"
+
+void* AllocatorImpl::Alloc(const size_t n) noexcept
+{
+	return MemoryManagerAccesser::GetMemoryManager()->Allocate(n);
+}
+
+void AllocatorImpl::Dealloc(void* ptr, const size_t n) noexcept
+{
+	MemoryManagerAccesser::GetMemoryManager()->Deallocate(ptr, n);
+}

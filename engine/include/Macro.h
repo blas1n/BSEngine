5f776a1d0ff4d885d382fbcfe1ead8b0f9596b72
blAs1N <qazasa123@gmail.com>
2019-11-18 22:14:15 +0900

    Wrapping string

--- engine/include/Macro.h
+++ engine/include/Macro.h
@@ -4,14 +4,7 @@
 
 #define BS_API
 
-#ifdef _UNICODE
-using tchar = wchar_t;
-#define TEXT(x) L##x
-#else
-using tchar = char;
-#define TEXT(x) x
-#endif
-
-#define static_check(expr) { static_assert(expr); }
-
 #define check(expr) { assert(expr); }
+
+#undef TEXT
+#define TEXT(x) u8##x

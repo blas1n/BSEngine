f17a483a62539cc4f21565b224b29e9fce1192bc
blAs1N <qazasa123@gmail.com>
2019-11-21 17:11:17 +0900

    Implement heap memory

--- engine/include/HeapMemory.h
+++ engine/include/HeapMemory.h
@@ -7,12 +7,21 @@ namespace BE
 	class BS_API HeapMemory final
 	{
 	public:
-		constexpr HeapMemory() noexcept {}
+		constexpr HeapMemory() noexcept;
 
-		void Init(void* const inMemory, const size_t inSize) noexcept {}
+		void Init(void* const inMemory, const size_t inSize) noexcept;
 
-		void* Allocate(const size_t size) { return nullptr; }
+		void Release() noexcept;
 
-		void Deallocate(void* const ptr, const size_t size) {}
+		void* Allocate(const size_t size);
+
+		void Deallocate(void* const ptr, const size_t size);
+
+	private:
+		Uint8* memory;
+		Uint8* marker;
+
+		size_t curNum;
+		size_t maxNum;
 	};
 }

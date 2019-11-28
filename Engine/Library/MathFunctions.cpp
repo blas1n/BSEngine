ff390c002066bdd34b0c6d3f9353e69c90fd1869
blAs1N <qazasa123@gmail.com>
2019-11-28 18:09:03 +0900

    Chnage exception interface

--- Engine/Library/MathFunctions.cpp
+++ Engine/Library/MathFunctions.cpp
@@ -5,32 +5,32 @@
 
 namespace BE::Math
 {
-	bool NearEqual(const Vector2& lhs, const Vector2& rhs, const float epsilon /*= MACHINE_EPSILON*/)
+	bool NearEqual(const Vector2& lhs, const Vector2& rhs, const float epsilon /*= MACHINE_EPSILON*/) noexcept
 	{
 		return (lhs - rhs).LengthSquared() <= Pow(epsilon);
 	}
 
-	bool NearEqual(const Vector3& lhs, const Vector3& rhs, const float epsilon /*= MACHINE_EPSILON*/)
+	bool NearEqual(const Vector3& lhs, const Vector3& rhs, const float epsilon /*= MACHINE_EPSILON*/) noexcept
 	{
 		return (lhs - rhs).LengthSquared() <= Pow(epsilon);
 	}
 
-	bool NearEqual(const Vector4& lhs, const Vector4& rhs, const float epsilon /*= MACHINE_EPSILON*/)
+	bool NearEqual(const Vector4& lhs, const Vector4& rhs, const float epsilon /*= MACHINE_EPSILON*/) noexcept
 	{
 		return (lhs - rhs).LengthSquared() <= Pow(epsilon);
 	}
 
-	Vector2 Lerp(const Vector2& a, const Vector2& b, const float delta)
+	Vector2 Lerp(const Vector2& a, const Vector2& b, const float delta) noexcept
 	{
 		return a + delta * (b - a);
 	}
 
-	Vector3 Lerp(const Vector3& a, const Vector3& b, const float delta)
+	Vector3 Lerp(const Vector3& a, const Vector3& b, const float delta) noexcept
 	{
 		return a + delta * (b - a);
 	}
 
-	Vector4 Lerp(const Vector4& a, const Vector4& b, const float delta)
+	Vector4 Lerp(const Vector4& a, const Vector4& b, const float delta) noexcept
 	{
 		return a + delta * (b - a);
 	}

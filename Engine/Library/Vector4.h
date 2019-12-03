17c5be08e2ee8e6ca69f9263dbc65c4de952dd5d
blAs1N <qazasa123@gmail.com>
2019-12-03 16:04:13 +0900

    Wrapping vector and matrix

--- Engine/Library/Vector4.h
+++ Engine/Library/Vector4.h
@@ -1,207 +1,163 @@
 #pragma once
 
 #include "Core.h"
+#include <Eigen/Dense>
 
 namespace BE::Math
 {
-	/// @todo Use SIMD register.
-	class BS_API Vector4 {
+	class BS_API Vector4
+	{
 	public:
-		static const Vector4 Zero;
-		static const Vector4 One;
-		static const Vector4 UnitX;
-		static const Vector4 UnitY;
-		static const Vector4 UnitZ;
-		static const Vector4 UnitW;
+		Vector4() noexcept : vec{ } {}
 
-		float x;
-		float y;
-		float z;
-		float w;
+		explicit Vector4(const float inX, const float inY) noexcept
+			: vec{ inX, inY } {}
 
-		constexpr Vector4() noexcept;
-		constexpr Vector4(float inX, float inY, float inZ, float inW) noexcept;
-		Vector4(float* elems) noexcept;
+		explicit Vector4(const float elems[4]) noexcept
+			: vec{ elems } {}
 
-		constexpr void Set(float inX, float inY, float inZ, float inW) noexcept;
-
-		constexpr float& operator[](Uint8 index) noexcept;
-		constexpr const float& operator[](Uint8 index) const noexcept;
-
-		constexpr Vector4 operator+() const noexcept;
-		constexpr Vector4 operator-() const noexcept;
-
-		constexpr Vector4& operator+=(const Vector4& other) noexcept;
-		constexpr Vector4& operator-=(const Vector4& other) noexcept;
-		constexpr Vector4& operator*=(const Vector4& other) noexcept;
-		constexpr Vector4& operator*=(const float scalar) noexcept;
-		constexpr Vector4& operator/=(const Vector4& other) noexcept;
-		constexpr Vector4& operator/=(const float scalar) noexcept;
-
-		float LengthSquared() const noexcept;
-		float Length() const noexcept;
-
-		void Normalized() noexcept;
-		static Vector4 Normalize(const Vector4& vec) noexcept;
-
-		static float Dot(const Vector4& lhs, const Vector4& rhs) noexcept;
-
-		// freinds
-		friend constexpr Vector4 operator+(const Vector4& lhs, const Vector4& rhs) noexcept;
-		friend constexpr Vector4 operator-(const Vector4& lhs, const Vector4& rhs) noexcept;
-		friend constexpr Vector4 operator*(const Vector4& lhs, const Vector4& rhs) noexcept;
-		friend constexpr Vector4 operator*(const Vector4& vec, const float scalar) noexcept;
-		friend constexpr Vector4 operator*(const float scalar, const Vector4& vec) noexcept;
-		friend constexpr Vector4 operator/(const Vector4& lhs, const Vector4& rhs) noexcept;
-		friend constexpr Vector4 operator/(const Vector4& vec, const float scalar) noexcept;
-
-		/// @warning Do not use it as an operator for the underlying API.
-		explicit operator const float* () const noexcept
+		inline void Set(const float inX, const float inY) noexcept
 		{
-			return &x;
+			vec << inX, inY;
 		}
+
+		inline float& operator[](const Uint8 index) noexcept
+		{
+			return vec[index];
+		}
+
+		inline const float& operator[](const Uint8 index) const noexcept
+		{
+			return vec[index];
+		}
+
+		inline Vector4 operator+() const noexcept
+		{
+			return *this;
+		}
+
+		inline Vector4 operator-() const noexcept
+		{
+			return *this * -1.0f;
+		}
+
+		inline Vector4& operator+=(const Vector4& other) noexcept
+		{
+			vec += other.vec;
+			return *this;
+		}
+
+		inline Vector4& operator-=(const Vector4& other) noexcept
+		{
+			vec -= other.vec;
+			return *this;
+		}
+
+		inline Vector4& operator*=(const Vector4& other) noexcept
+		{
+			vec[0] *= other.vec[0];
+			vec[1] *= other.vec[1];
+			vec[2] *= other.vec[2];
+			vec[3] *= other.vec[3];
+			return *this;
+		}
+
+		inline Vector4& operator*=(const float scalar) noexcept
+		{
+			vec *= scalar;
+			return *this;
+		}
+
+		inline Vector4& operator/=(const Vector4& other) noexcept
+		{
+			vec[0] /= other.vec[0];
+			vec[1] /= other.vec[1];
+			vec[2] /= other.vec[2];
+			vec[3] /= other.vec[3];
+			return *this;
+		}
+
+		inline Vector4& operator/=(const float scalar) noexcept
+		{
+			vec /= scalar;
+			return *this;
+		}
+
+		inline float LengthSquared() const noexcept
+		{
+			vec.squaredNorm();
+		}
+
+		inline float Length() const noexcept
+		{
+			vec.norm();
+		}
+
+		inline void Normalize() noexcept
+		{
+			vec.normalize();
+		}
+
+		inline Vector4 Normalized() const noexcept
+		{
+			Vector4 ret;
+			ret.vec = vec.normalized();
+			return ret;
+		}
+
+		static inline float Dot(const Vector4& lhs, const Vector4& rhs) noexcept
+		{
+			return lhs.vec.dot(rhs.vec);
+		}
+
+		static inline Vector4 Cross3(const Vector4& lhs, const Vector4& rhs) noexcept
+		{
+			Vector4 ret;
+			ret.vec = lhs.vec.cross3(rhs.vec);
+			return ret;
+		}
+
+		static inline Vector4 Reflect(const Vector4& v, const Vector4& n) noexcept
+		{
+			return v - 2.0f * Vector4::Dot(v, n) * n;
+		}
+
+	private:
+		Eigen::Vector4f vec;
 	};
 
-	inline constexpr Vector4::Vector4() noexcept
-		: x(0.0f), y(0.0f), z(0.0f), w(0.0f) {}
-
-	inline constexpr Vector4::Vector4(const float inX, const float inY, const float inZ, const float inW) noexcept
-		: x(inX), y(inY), z(inZ), w(inW) {}
-
-	inline Vector4::Vector4(float* elems) noexcept
-		: x(elems[0]), y(elems[1]), z(elems[2]), w(elems[3]) {}
-
-	constexpr inline void Vector4::Set(const float inX, const float inY, const float inZ, const float inW) noexcept
+	Vector4 operator+(Vector4 lhs, const Vector4& rhs) noexcept
 	{
-		x = inX;
-		y = inY;
-		z = inZ;
-		w = inW;
+		return lhs += rhs;
 	}
 
-	constexpr inline float& Vector4::operator[](Uint8 index) noexcept
+	Vector4 operator-(Vector4 lhs, const Vector4& rhs) noexcept
 	{
-		check(index < 5);
-		return (&x)[index];
+		return lhs += rhs;
 	}
 
-	constexpr inline const float& Vector4::operator[](Uint8 index) const noexcept
+	Vector4 operator*(Vector4 lhs, const Vector4& rhs) noexcept
 	{
-		check(index < 5);
-		return (&x)[index];
+		return lhs += rhs;
 	}
 
-	constexpr inline Vector4 Vector4::operator+() const noexcept
+	Vector4 operator*(Vector4 vec, const float scalar) noexcept
 	{
-		return *this;
+		return vec *= scalar;
 	}
 
-	constexpr inline Vector4 Vector4::operator-() const noexcept
+	Vector4 operator*(const float scalar, Vector4 vec) noexcept
 	{
-		return *this * -1.0f;
+		return vec *= scalar;
 	}
 
-	constexpr inline Vector4& Vector4::operator+=(const Vector4& other) noexcept
+	Vector4 operator/(Vector4 lhs, const Vector4& rhs) noexcept
 	{
-		x += other.x;
-		y += other.y;
-		z += other.z;
-		w += other.w;
-		return *this;
+		return lhs /= rhs;
 	}
 
-	constexpr inline Vector4& Vector4::operator-=(const Vector4& other) noexcept
+	Vector4 operator/(Vector4 vec, const float scalar) noexcept
 	{
-		x -= other.x;
-		y -= other.y;
-		z -= other.z;
-		w -= other.w;
-		return *this;
-	}
-
-	constexpr inline Vector4& Vector4::operator*=(const Vector4& other) noexcept
-	{
-		x *= other.x;
-		y *= other.y;
-		z *= other.z;
-		w *= other.w;
-		return *this;
-	}
-
-	constexpr inline Vector4& Vector4::operator*=(const float scalar) noexcept
-	{
-		x *= scalar;
-		y *= scalar;
-		z *= scalar;
-		w *= scalar;
-		return *this;
-	}
-
-	constexpr inline Vector4& Vector4::operator/=(const Vector4& other) noexcept
-	{
-		x /= other.x;
-		y /= other.y;
-		z /= other.z;
-		w /= other.w;
-		return *this;
-	}
-
-	constexpr inline Vector4& Vector4::operator/=(const float scalar) noexcept
-	{
-		x /= scalar;
-		y /= scalar;
-		z /= scalar;
-		w /= scalar;
-		return *this;
-	}
-
-	inline void Vector4::Normalized() noexcept
-	{
-		*this /= Length();
-	}
-
-	inline Vector4 Vector4::Normalize(const Vector4& vec) noexcept
-	{
-		return vec / vec.Length();
-	}
-
-	inline float Vector4::Dot(const Vector4& lhs, const Vector4& rhs) noexcept
-	{
-		return (lhs.x * rhs.x) + (lhs.y * rhs.y) + (lhs.z * rhs.z) + (lhs.w * rhs.w);
-	}
-
-	constexpr inline Vector4 operator+(const Vector4& lhs, const Vector4& rhs) noexcept
-	{
-		return Vector4{ lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z, lhs.w + rhs.w };
-	}
-
-	constexpr inline Vector4 operator-(const Vector4& lhs, const Vector4& rhs) noexcept
-	{
-		return Vector4{ lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z, lhs.w - rhs.w };
-	}
-
-	constexpr inline Vector4 operator*(const Vector4& lhs, const Vector4& rhs) noexcept
-	{
-		return Vector4{ lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z, lhs.w * rhs.w };
-	}
-
-	constexpr inline Vector4 operator*(const Vector4& vec, const float scalar) noexcept
-	{
-		return Vector4{ vec.x * scalar, vec.y * scalar, vec.z * scalar, vec.w * scalar };
-	}
-
-	constexpr inline Vector4 operator*(const float scalar, const Vector4& vec) noexcept
-	{
-		return Vector4{ vec.x * scalar, vec.y * scalar, vec.z * scalar, vec.w * scalar };
-	}
-
-	constexpr inline Vector4 operator/(const Vector4& lhs, const Vector4& rhs) noexcept
-	{
-		return Vector4{ lhs.x / rhs.x, lhs.y / rhs.y, lhs.z / rhs.z, lhs.w / rhs.w };
-	}
-
-	constexpr inline Vector4 operator/(const Vector4& vec, const float scalar) noexcept
-	{
-		return Vector4{ vec.x / scalar, vec.y / scalar, vec.z / scalar, vec.w / scalar };
+		return vec /= scalar;
 	}
 }

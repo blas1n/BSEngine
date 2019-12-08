2e55d38f934a335b32cf5cf842bba3148c195b69
blAs1N <qazasa123@gmail.com>
2019-12-08 22:57:29 +0900

    Delete iterator concept in array

--- Engine/Library/Array.cpp
+++ Engine/Library/Array.cpp
@@ -6,103 +6,125 @@
 namespace BE
 {
 	template <class T, class InAllocator>
-	void Array<T, InAllocator>::Insert(const Array<T, InAllocator>& other, ConstIterator pos)
+	void Array<T, InAllocator>::Insert(const Array<T, InAllocator>& other, const SizeType pos)
 	{
 		if (IsEmpty())
-			arr = other.arr;
+			container = other.container;
 		else
-			Insert(other.Begin(), other.End(), pos);
+			container.insert(container.cbegin() + pos,
+				other.container.cbegin(), other.container.cend());
 	}
 
 	template <class T, class InAllocator>
-	void Array<T, InAllocator>::Insert(Array<T, InAllocator>&& other, ConstIterator pos)
+	void Array<T, InAllocator>::Insert(Array<T, InAllocator>&& other, const SizeType pos)
 	{
 		if (IsEmpty())
 		{
-			arr = std::move(other.arr);
+			container = std::move(other.container);
 			return;
 		}
 
 
 		Reserve(GetSize() + other.GetSize());
-		Append(pos, CEnd());
-		std::move(other.Begin(), other.End(), std::inserter(arr, pos));
+		Append(pos, GetSize());
+		std::move(other.Begin(), other.End(), std::inserter(container, pos));
 		other.Clear();
 	}
 
 	template <class T, class InAllocator>
-	void Array<T, InAllocator>::Insert(const T& item, ConstIterator pos)
+	void Array<T, InAllocator>::Insert(const T& item, const SizeType pos)
 	{
-		arr.insert(pos, item);
+		container.insert(container.cbegin() + pos, item);
 	}
 
 	template <class T, class InAllocator>
-	void Array<T, InAllocator>::Insert(T&& item, ConstIterator pos)
+	void Array<T, InAllocator>::Insert(T&& item, const SizeType pos)
 	{
-		arr.insert(pos, std::move(item));
+		container.insert(container.cbegin() + pos, std::move(item));
 	}
 
 	template <class T, class InAllocator>
-	void Array<T, InAllocator>::Insert(std::initializer_list<T> elems, ConstIterator pos)
+	void Array<T, InAllocator>::Insert(std::initializer_list<T> elems, const SizeType pos)
 	{
-		arr.insert(pos, elems);
+		container.insert(container.cbegin() + pos, elems);
 	}
 
 	template <class T, class InAllocator>
-	void Array<T, InAllocator>::Insert(T* ptr, SizeType count, ConstIterator pos)
+	void Array<T, InAllocator>::Insert(T* ptr, SizeType count, const SizeType pos)
 	{
-		arr.insert(pos, ptr, ptr + count);
+		container.insert(container.cbegin() + pos, ptr, ptr + count);
 	}
 
 	template <class T, class InAllocator>
 	void Array<T, InAllocator>::AddUnique(const T& item)
 	{
-		if (std::find(CBegin(), CEnd(), item) == CEnd())
+		const auto begin = container.cbegin();
+		const auto end = container.cend();
+
+		if (std::find(begin + , end, item) == end)
 			Add(item);
 	}
 
 	template <class T, class InAllocator>
 	void Array<T, InAllocator>::AddUnique(T&& item)
 	{
-		if (std::find(CBegin(), CEnd(), item) == CEnd())
+		const auto begin = container.cbegin();
+		const auto end = container.cend();
+
+		if (std::find(begin + , end, item) == end)
 			Add(std::move(item));
 	}
 
 	template <class T, class InAllocator>
 	void Array<T, InAllocator>::Remove(const T& item)
 	{
-		arr.erase(std::find(CBegin(), CEnd(), item));
+		const auto begin = container.cbegin();
+		const auto end = container.cend();
+
+		container.erase(std::find(begin, end, item));
 	}
 
 	template <class T, class InAllocator>
 	void Array<T, InAllocator>::RemoveLast(const T& item)
 	{
-		arr.erase(std::find(CRBegin(), CREnd(), item));
+		const auto begin = container.crbegin();
+		const auto end = container.crend();
+
+		container.erase(std::find(begin, end, item));
 	}
 
 	template <class T, class InAllocator>
 	SizeType Array<T, InAllocator>::RemoveAll(const T& item) noexcept
 	{
-		const auto iter = std::remove(CBegin(), CEnd(), item);
-		const auto removeNum = CEnd() - iter;
-		if (removeNum > 0) arr.erase(iter, CEnd());
+		const auto begin = container.cbegin();
+		const auto end = container.cend();
+
+		const auto iter = std::remove(begin, end, item);
+		const auto removeNum = end - iter;
+		if (removeNum > 0) container.erase(iter, end);
 		return removeNum;
 	}
 
 	template <class T, class InAllocator>
-	void Array<T, InAllocator>::RemoveAt(SizeType index, SizeType count/* = 1*/)
+	void Array<T, InAllocator>::RemoveAt(const SizeType index, const SizeType count/* = 1*/)
 	{
-		auto erasePos = CBegin() + index;
-		arr.erase(erasePos, erasePos + count);
+		const auto begin = container.cbegin();
+		const auto end = container.cend();
+
+		auto erasePos = begin + index;
+		container.erase(erasePos, erasePos + count);
 	}
 
 	template <class T, class InAllocator>
 	template <class Predicate>
 	SizeType Array<T, InAllocator>::RemoveByPredicate(Predicate&& pred) noexcept
 	{
-		const auto iter = std::remove_if(CBegin(), CEnd(), std::forward<Predicate>(pred));
-		const auto removeNum = CEnd() - iter;
-		if (removeNum > 0) arr.erase(iter, CEnd());
+		const auto begin = container.cbegin();
+		const auto end = container.cend();
+
+		const auto iter = std::remove_if(begin, end, std::forward<Predicate>(pred));
+		const auto removeNum = end - iter;
+		if (removeNum > 0) container.erase(iter, end);
 		return removeNum;
 	}
 }
--- Engine/Library/Array.h
+++ Engine/Library/Array.h
@@ -15,12 +15,6 @@ namespace BE
 		using ElementType = T;
 		using AllocatorType = InAllocator;
 
-		using Iterator = typename ContainerType::iterator;
-		using ConstIterator = typename ContainerType::const_iterator;
-
-		using ReverseIterator = typename ContainerType::reverse_iterator;
-		using ConstReverseIterator = typename ContainerType::const_reverse_iterator;
-
 		Array() noexcept = default;
 		Array(const Array& other) = default;
 		Array(Array&& other) noexcept = default;
@@ -54,50 +48,40 @@ namespace BE
 			return *this;
 		}
 
-		inline bool operator==(const Array& other) const noexcept { return container == other.container; }
-		inline bool operator!=(const Array& other) const noexcept { return container != other.container; }
-
 		inline T& operator[](SizeType index) { return container.at(index); }
 		inline const T& operator[](SizeType index) const { return container.at(index); }
 
-		void Insert(const Array& other, ConstIterator pos);
-		void Insert(Array&& other, ConstIterator pos);
+		void Insert(const Array& other, SizeType pos);
+		void Insert(Array&& other, SizeType pos);
 
-		void Insert(const T& item, ConstIterator pos);
-		void Insert(T&& item, ConstIterator pos);
+		void Insert(const T& item, SizeType pos);
+		void Insert(T&& item, SizeType pos);
 
-		void Insert(std::initializer_list<T> elems, ConstIterator pos);
-		void Insert(T* ptr, SizeType count, ConstIterator pos);
+		void Insert(std::initializer_list<T> elems, SizeType pos);
+		void Insert(T* ptr, SizeType count, SizeType pos);
 
-		template <class IteratorType>
-		void Insert(IteratorType begin, IteratorType end, ConstIterator pos);
-
-		inline void Add(const T& item) { Insert(item, End()); }
-		inline void Add(T&& item) { Insert(item, End()); }
+		inline void Add(const T& item) { Insert(item, GetSize()); }
+		inline void Add(T&& item) { Insert(item, GetSize()); }
 
 		void AddUnique(const T& item);
 		void AddUnique(T&& item);
 
-		inline void Append(std::initializer_list<T> elems) { Insert(elems, End()); }
-		inline void Append(const T* ptr, SizeType count) { Insert(ptr, count, End()); }
+		inline void Append(std::initializer_list<T> elems) { Insert(elems, GetSize()); }
+		inline void Append(const T* ptr, SizeType count) { Insert(ptr, count, GetSize()); }
 
-		inline void Append(const Array& other) { Insert(other, End()); }
-		inline void Append(Array&& other) { Insert(std::move(other), End()); }
-
-		template <class Iterator>
-		inline void Array<T, InAllocator>::Insert(Iterator begin, Iterator end) { Insert(begin, end); }
-
+		inline void Append(const Array& other) { Insert(other, GetSize()); }
+		inline void Append(Array&& other) { Insert(std::move(other), GetSize()); }
+	
 		template <class... Args>
-		inline Iterator Emplace(Args&&... args)
+		inline void Emplace(Args&&... args)
 		{
 			container.emplace_back(std::forward<Args>(args)...);
-			return --End();
 		}
 
 		template <class... Args>
-		inline Iterator EmplaceAt(ConstIterator pos, Args&&... args)
+		inline void EmplaceAt(SizeType pos, Args&&... args)
 		{
-			return container.emplace(pos, std::forward<Args>(args)...);
+			container.emplace(container.cbegin() + pos, std::forward<Args>(args)...);
 		}
 
 		void Remove(const T& item);
@@ -113,22 +97,6 @@ namespace BE
 		inline void Resize(SizeType size, const T& value = T()) { container.resize(size, value); }
 		inline void Reserve(SizeType size) { container.reserve(size); }
 
-		inline Iterator Begin() noexcept { return container.begin(); }
-		inline ConstIterator Begin() const noexcept { return container.begin(); }
-		inline ConstIterator CBegin() const noexcept { return container.cbegin(); }
-
-		inline Iterator End() noexcept { return container.end(); }
-		inline ConstIterator End() const noexcept { return container.end(); }
-		inline ConstIterator CEnd() const noexcept { return container.cend(); }
-
-		inline ReverseIterator RBegin() noexcept { return container.rbegin(); }
-		inline ConstReverseIterator RBegin() const noexcept { return container.rbegin(); }
-		inline ConstReverseIterator CRBegin() const noexcept { return container.crbegin(); }
-
-		inline ReverseIterator REnd() noexcept { return container.rend(); }
-		inline ConstReverseIterator REnd() const noexcept { return container.rend(); }
-		inline ConstReverseIterator CREnd() const noexcept { return container.crend(); }
-
 		inline bool IsEmpty() const noexcept { return container.empty(); }
 		
 		inline SizeType GetSize() const noexcept { return container.size(); }
@@ -138,11 +106,12 @@ namespace BE
 
 		inline void Clear() noexcept { container.clear(); }
 
-		inline Iterator begin() noexcept { return container.begin(); }
-		inline ConstIterator begin() const noexcept { return container.begin(); }
+		// Don't use! Only range based for for the function.
+		inline typename ContainerType::iterator begin() noexcept { return container.begin(); }
+		inline typename ContainerType::const_iterator begin() const noexcept { return container.begin(); }
 
-		inline Iterator end() noexcept { return container.end(); }
-		inline ConstIterator end() const noexcept { return container.end(); }
+		inline typename ContainerType::iterator end() noexcept { return container.end(); }
+		inline typename ContainerType::const_iterator end() const noexcept { return container.end(); }
 
 	private:
 		friend bool operator==(const Array& lhs, const Array& rhs) noexcept;
@@ -178,12 +147,5 @@ namespace BE
 		Reserve(other.GetSize());
 		for (auto&& elem : std::move(other))
 			Emplace(std::move(elem));
-	}
-
-	template <class T, class InAllocator>
-	template <class IteratorType>
-	void Array<T, InAllocator>::Insert(IteratorType begin, IteratorType end, ConstIterator pos)
-	{
-		container.insert(pos, begin, end);
 	}
 }

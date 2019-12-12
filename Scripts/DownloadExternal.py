44c424f3dc38fd930051ddd5092ecc80594b1c50
blAs1N <qazasa123@gmail.com>
2019-12-12 16:41:26 +0900

    Apply feedback of codacy

--- Scripts/DownloadExternal.py
+++ Scripts/DownloadExternal.py
@@ -11,20 +11,20 @@ def make_abs_dir(name):
     abs_dir = os.path.join(os.path.dirname(os.getcwd()), 'External')
     return os.path.join(abs_dir, name)
 
-def download(url, dir):
-    path, name = os.path.split(dir)
+def download(url, file_dir):
+    path, name = os.path.split(file_dir)
     name = os.path.splitext(os.path.splitext(name)[0])[0]
 
     print(f'Downloading {name}...')
-    with open(dir, "wb") as file:
+    with open(file_dir, "wb") as file:
         response = get(url)
         file.write(response.content)
 
     print(f'Extracting {name}...')
-    with tarfile.open(dir) as file:
+    with tarfile.open(file_dir) as file:
         file.extractall(path)
 
-    os.remove(dir)
+    os.remove(file_dir)
     print(f'{name} download complete.')
 
 if __name__ == '__main__':

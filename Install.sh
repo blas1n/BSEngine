dd6ae4fe31edc1b3464e0aeaaba783a54e4e5293
blAs1N <qazasa123@gmail.com>
2019-12-13 10:11:35 +0900

    Some change script

--- Install.sh
+++ Install.sh
@@ -3,12 +3,12 @@
 set -e
 
 while read package
-    do
-        if [[ ! "$(pip freeze)" =~ "$(package)" ]]; then
-            echo "Now install $(package)"
-            pip install $(package)
-        fi
-    done < requirement.txt
+do
+    if [[ ! "$(pip freeze)" =~ "$(package)" ]]; then
+        echo "Now install $(package)"
+        pip install $(package)
+    fi
+done < requirement.txt
 
 cd Scripts
 python3 DownloadExternal.py

3776a19da72c01fbe84cea0d558603d3ce27d4c8
blAs1N <qazasa123@gmail.com>
2019-12-14 10:30:38 +0900

    Change command

--- Install.bat
+++ Install.bat
@@ -4,7 +4,7 @@ for /f "delims=" %%i in (requirement.txt) do (
     echo %%i
     if not "x!pip freeze:%%i=!"=="x%pip freeze%"  (
         echo Now install %%i
-        pip install requests
+        py -m pip install %%i
     )
 )
 

8222fcc43e2672c02e1b96c58be861d0355dc9a0
blAs1N <qazasa123@gmail.com>
2019-12-17 23:55:06 +0900

    Change windows python command

--- Scripts/bpm.bat
+++ Scripts/bpm.bat
@@ -1,26 +1,26 @@
 @echo off
 
 if "%1" == "init" (
-    python3 -c "import bpm;bpm.init()"
+    py -c "import bpm;bpm.init()"
     goto :EOF
 )
 if "%1" == "register" (
-    python3 -c "import bpm;bpm.register('%2', '%3')"
+    py -c "import bpm;bpm.register('%2', '%3')"
     goto :EOF
 )
 if "%1" == "unregister" (
-    python3 -c "import bpm;bpm.unregister('%2')"
+    py -c "import bpm;bpm.unregister('%2')"
     goto :EOF
 )
 if "%1" == "install" (
-    python3 -c "import bpm;bpm.install('%2')"
+    py -c "import bpm;bpm.install('%2')"
     goto :EOF
 )
 if "%1" == "uninstall" (
-    python3 -c "import bpm;bpm.uninstall('%2')"
+    py -c "import bpm;bpm.uninstall('%2')"
     goto :EOF
 )
 if "%1" == "search" (
-    python3 -c "import bpm;bpm.print_search('%2')"
+    py -c "import bpm;bpm.print_search('%2')"
     goto :EOF
 )

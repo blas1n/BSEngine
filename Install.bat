3620d1ffdaa504bd3e9f63d701fc1caaf6a69524
blAs1N <qazasa123@gmail.com>
2019-12-17 23:55:06 +0900

    Change windows python command

--- Install.bat
+++ Install.bat
@@ -3,7 +3,7 @@
 for /f "delims=" %%i in (Config/Requirement.txt) do (
     if "x!pip freeze:%%i=!"=="x%pip freeze%"  (
         echo Now install %%i
-        python3 -m pip install %%i
+        py -m pip install %%i
     )
 )
 

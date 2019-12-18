4b23428cb7e994152cc6eaecac8f16f9eacaed87
blAs1N <qazasa123@gmail.com>
2019-12-18 22:40:18 +0900

    Use simple commands

--- Install.bat
+++ Install.bat
@@ -1,11 +1,6 @@
 @echo off
 
-for /f "delims=" %%i in (Config/Requirement.txt) do (
-    if "x!pip freeze:%%i=!"=="x%pip freeze%"  (
-        echo Now install %%i
-        py -3 -m pip install %%i
-    )
-)
+py -3 -m pip install -r Config/Requirement.txt
 
 cd Scripts
 bpm install all

843e8ffcaa41aa958d73398ea87109f1bfd1ca8f
blAs1N <qazasa123@gmail.com>
2019-12-19 16:22:35 +0900

    Change if command to standard

--- Scripts/bpm.sh
+++ Scripts/bpm.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-if [[ "$1" == "init" ]]; then
+if [ "$1" = "init" ]; then
     python3 -c "import bpm;bpm.init('$2')"
 fi
-if [[ "$1" == "register" ]]; then
+if [ "$1" = "register" ]; then
     python3 -c "import bpm;bpm.register('$2', '$3')"
 fi
-if [[ "$1" == "unregister" ]]; then
+if [ "$1" = "unregister" ]; then
     python3 -c "import bpm;bpm.unregister('$2')"
 fi
-if [[ "$1" == "install" ]]; then
+if [ "$1" = "install" ]; then
     python3 -c "import bpm;bpm.install('$2')"
 fi
-if [[ "$1" == "uninstall" ]]; then
+if [ "$1" = "uninstall" ]; then
     python3 -c "import bpm;bpm.uninstall('$2')"
 fi
-if [[ "$1" == "search" ]]; then
+if [ "$1" = "search" ]; then
     python3 -c "import bpm;bpm.print_search('$2')"
 fi

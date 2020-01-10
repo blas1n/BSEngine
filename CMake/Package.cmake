4559045aaa8b2a4a1872e6d3fa63ac6d34017199
blAs1N <qazasa123@gmail.com>
2020-01-10 22:22:01 +0900

    Set cmake

--- CMake/Package.cmake
+++ CMake/Package.cmake
@@ -1,6 +1,3 @@
-list (APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/CMake/Modules)
-set (CMAKE_INSTALL_PREFIX ${CMAKE_SOURCE_DIR}/External)
-
 if (CMAKE_SYSTEM_NAME MATCHES "Linux")
 	find_package (Threads REQUIRED)
 endif ()

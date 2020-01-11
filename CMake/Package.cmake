de8430e9fbc23eab6de2bee78553a8e52e8a3739
blAs1N <qazasa123@gmail.com>
2020-01-11 17:47:42 +0900

    Set SDL,

--- CMake/Package.cmake
+++ CMake/Package.cmake
@@ -2,4 +2,5 @@ if (CMAKE_SYSTEM_NAME MATCHES "Linux")
 	find_package (Threads REQUIRED)
 endif ()
 
-find_package (Eigen3 REQUIRED)
+find_package (Eigen3 CONFIG REQUIRED)
+find_package (SDL2 CONFIG REQUIRED)

241740f18aee96a8c7c9a1750d51c2e8b0ec9085
blAs1N <qazasa123@gmail.com>
2020-11-26 10:51:31 +0900

    Remove unused macro

--- CMake/Util.cmake
+++ CMake/Util.cmake
@@ -17,21 +17,3 @@ macro (register_module)
         target_precompile_headers (${MODULE_ID} PRIVATE Private/pch.h)
     endif ()
 endmacro ()
-
-macro (add_public_dependencies)
-	target_link_libraries (${PROJECT_ID} PUBLIC ${ARGN})
-#	get_filename_component (PARENT_DIR ${CMAKE_CURRENT_SOURCE_DIR} DIRECTORY)
-
-#	foreach (ARG ${ARGN})
-#		target_include_directories (${PROJECT_ID} PUBLIC ${PARENT_DIR}/${ARG})
-#	endforeach ()
-endmacro ()
-
-macro (add_private_dependencies)
-	target_link_libraries (${PROJECT_ID} PRIVATE ${ARGN})
-#	get_filename_component (PARENT_DIR ${CMAKE_CURRENT_SOURCE_DIR} DIRECTORY)
-
-#	foreach (ARG ${ARGN})
-#		target_include_directories (${PROJECT_ID} PRIVATE ${PARENT_DIR}/${ARG})
-#	endforeach ()
-endmacro ()

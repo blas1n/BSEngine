5902fb103ea33036ee14b158559b7727ded5f43f
blAs1N <qazasa123@gmail.com>
2020-11-26 10:51:24 +0900

    Set cmake variable's name

--- CMake/Util.cmake
+++ CMake/Util.cmake
@@ -1,20 +1,20 @@
 macro (register_module)
     get_filename_component (MODULE_NAME ${CMAKE_CURRENT_SOURCE_DIR} NAME)
-    set (PROJECT_ID ${CMAKE_PROJECT_NAME}-${MODULE_NAME})
+    set (MODULE_ID ${CMAKE_PROJECT_NAME}-${MODULE_NAME})
 
     file (GLOB_RECURSE SOURCES CONFIGURE_DEPENDS "Private/*.cpp")
-    add_library (${PROJECT_ID} SHARED ${SOURCES})
-    target_include_directories (${PROJECT_ID} PUBLIC "Public")
+    add_library (${MODULE_ID} SHARED ${SOURCES})
+    target_include_directories (${MODULE_ID} PUBLIC "Public")
 
     string (TOUPPER ${MODULE_NAME}_API API)
-    target_compile_definitions (${PROJECT_ID} PRIVATE ${API}=${DLL_EXPORT})
+    target_compile_definitions (${MODULE_ID} PRIVATE ${API}=${DLL_EXPORT})
 
     if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/Public/pch.h")
-        target_precompile_headers (${PROJECT_ID} PUBLIC Public/pch.h)
+        target_precompile_headers (${MODULE_ID} PUBLIC Public/pch.h)
     endif ()
 
     if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/Private/pch.h")
-        target_precompile_headers (${PROJECT_ID} PRIVATE Private/pch.h)
+        target_precompile_headers (${MODULE_ID} PRIVATE Private/pch.h)
     endif ()
 endmacro ()
 

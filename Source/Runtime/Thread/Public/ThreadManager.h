7aa4397995736c61da74a7dbb84b2a1f2bbefcdc
blAs1N <qazasa123@gmail.com>
2020-12-15 11:36:48 +0900

    Fix typo

--- Source/Runtime/Thread/Public/ThreadManager.h
+++ Source/Runtime/Thread/Public/ThreadManager.h
@@ -32,7 +32,7 @@ decltype(auto) ThreadManager::AddTask(Fn&& fn, Args&&... args) noexcept
 {
 	auto task = std::make_shared
 		<std::packaged_task<std::invoke_result_t<Fn, Args...>()>>(
-			std::bind(Forward<Fn>(fn), Forward<Args>(args)...)
+			std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...)
 		);
 
 	taskMutex.lock();

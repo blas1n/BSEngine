97d306bf3c4e4d0ce373f540833cfd72dcab23c5
blAs1N <qazasa123@gmail.com>
2021-03-31 14:51:56 +0900

    Use WinMain instead of SDL-Main

--- Source/Runtime/Launch/Launch.cpp
+++ Source/Runtime/Launch/Launch.cpp
@@ -1,4 +1,16 @@
-int main()
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include "Game.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
 {
-	return 0;
+	Game game;
+	auto ret = game.Init();
+
+	if (!ret)
+		ret = game.Run();
+
+	game.Release();
+	return ret;
 }

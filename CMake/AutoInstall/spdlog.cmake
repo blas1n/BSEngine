06aec9e0e57ebedf44994fcd619b576065eb049b
blAs1N <qazasa123@gmail.com>
2021-03-31 22:24:29 +0900

    Add spdlog, rapidJSON download cmake

---
+++ CMake/AutoInstall/spdlog.cmake
@@ -0,0 +1,17 @@
+cmake_minimum_required (VERSION 3.10)
+project (@name@-download NONE)
+include (ExternalProject)
+
+ExternalProject_Add (
+	@name@-download
+	GIT_REPOSITORY		https://github.com/gabime/spdlog
+	GIT_TAG				1.8.5
+	GIT_SHALLOW			TRUE
+	GIT_PROGRESS		TRUE
+	SOURCE_DIR			"${src_dir}"
+	BINARY_DIR			"${build_dir}"
+	CONFIGURE_COMMAND	""
+	BUILD_COMMAND		""
+	INSTALL_COMMAND		""
+	TEST_COMMAND		""
+)

ab83ed3b08e4976ad240a83d33be268bd2e7bd47
blAs1N <qazasa123@gmail.com>
2021-03-31 14:50:57 +0900

    Create auto third-party install system

---
+++ CMake/AutoInstall/fmt.cmake
@@ -0,0 +1,17 @@
+cmake_minimum_required (VERSION 3.10)
+project (@name@-download NONE)
+include (ExternalProject)
+
+ExternalProject_Add (
+	@name@-download
+	GIT_REPOSITORY		https://github.com/fmtlib/fmt
+	GIT_TAG				7.1.3
+	GIT_SHALLOW			TRUE
+	GIT_PROGRESS		TRUE
+	SOURCE_DIR			"${src_dir}"
+	BINARY_DIR			"${build_dir}"
+	CONFIGURE_COMMAND	""
+	BUILD_COMMAND		""
+	INSTALL_COMMAND		""
+	TEST_COMMAND		""
+)

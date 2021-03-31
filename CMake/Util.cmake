75250221234ed56e3e43232ab220dc668da07f1e
blAs1N <qazasa123@gmail.com>
2021-03-31 22:23:54 +0900

    Follow CMake name convention

--- CMake/Util.cmake
+++ CMake/Util.cmake
@@ -17,57 +17,57 @@ function (register_module)
     endif ()
 endfunction ()
 
-function (download_package name)
-    set (cmake_dir "${PROJECT_SOURCE_DIR}/CMake/AutoInstall")
-    set (src_dir "${PROJECT_SOURCE_DIR}/Source/ThirdParty/${name}")
-    set (build_dir "${CMAKE_BINARY_DIR}/ThirdParty/${name}-build")
-    set (download_dir "${CMAKE_BINARY_DIR}/ThirdParty/${name}-download")
-    configure_file ("${cmake_dir}/${name}.cmake" "${download_dir}/CMakeLists.txt" @ONLY)
-    
+function (download_package NAME)
+    set (CMAKE_DIR "${PROJECT_SOURCE_DIR}/CMake/AutoInstall")
+    set (SRC_DIR "${PROJECT_SOURCE_DIR}/Source/ThirdParty/${NAME}")
+    set (BUILD_DIR "${CMAKE_BINARY_DIR}/ThirdParty/${NAME}-build")
+    set (DOWNLOAD_DIR "${CMAKE_BINARY_DIR}/ThirdParty/${NAME}-download")
+    configure_file ("${CMAKE_DIR}/${NAME}.cmake" "${DOWNLOAD_DIR}/CMakeLists.txt" @ONLY)
+
     execute_process (
         COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
         RESULT_VARIABLE result
-        WORKING_DIRECTORY "${download_dir}"
+        WORKING_DIRECTORY "${DOWNLOAD_DIR}"
     )
 
     if (result)
-        message (FATAL_ERROR "CMake step for ${name} failed: ${result}")
+        message (FATAL_ERROR "CMake step for ${NAME} failed: ${result}")
     endif ()
 
     execute_process (
         COMMAND ${CMAKE_COMMAND} --build .
         RESULT_VARIABLE result
-        WORKING_DIRECTORY "${download_dir}"
+        WORKING_DIRECTORY "${DOWNLOAD_DIR}"
     )
 
     if (result)
-        message (FATAL_ERROR "Build step for ${name} failed: ${result}")
+        message (FATAL_ERROR "Build step for ${NAME} failed: ${result}")
     endif ()
 
-    if (EXISTS "${cmake_dir}/${name}-build.cmake")
-         configure_file ("${cmake_dir}/${name}-build.cmake" "${src_dir}/CMakeLists.txt" @ONLY)
+    if (EXISTS "${CMAKE_DIR}/${NAME}-build.cmake")
+         configure_file ("${CMAKE_DIR}/${NAME}-build.cmake" "${SRC_DIR}/CMakeLists.txt" @ONLY)
     endif ()
 
-    if (EXISTS "${src_dir}/CMakeLists.txt")
-         add_subdirectory ("${src_dir}" "${build_dir}" EXCLUDE_FROM_ALL)
+    if (EXISTS "${SRC_DIR}/CMakeLists.txt")
+         add_subdirectory ("${SRC_DIR}" "${BUILD_DIR}" EXCLUDE_FROM_ALL)
     endif ()
 endfunction ()
 
-function (get_package name)
+function (get_package NAME)
     if (${ARGC} GREATER 1)
          set(version ${ARGV1})
     endif ()
 
     if (version)
-        find_package (${name} ${version} QUIET)
+        find_package (${NAME} ${version} QUIET)
     else ()
-        find_package (${name} QUIET)
+        find_package (${NAME} QUIET)
     endif ()
     
-    if (${name}_FOUND)
-        message (STATUS "Found ${name} from system")
+    if (${NAME}_FOUND)
+        message (STATUS "Found ${NAME} from system")
     else ()
-        message (STATUS "Could not find ${name} from system. Downloading...")
-        download_package (${name})
+        message (STATUS "Could not find ${NAME} from system. Downloading...")
+        download_package (${NAME})
     endif ()
 endfunction ()

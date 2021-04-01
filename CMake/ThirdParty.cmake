339a114b590601cd92558ec33d6899a816bffd45
blAs1N <qazasa123@gmail.com>
2021-04-01 14:52:44 +0900

    Tolower in variable

--- CMake/ThirdParty.cmake
+++ CMake/ThirdParty.cmake
@@ -1,6 +1,6 @@
-find_package(SDL2 REQUIRED)
-find_package(GLEW REQUIRED)
+find_package (SDL2 REQUIRED)
+find_package (GLEW REQUIRED)
 
-get_package(fmt 7.1.3)
-get_package(spdlog 1.8.5)
-get_package(rapidJSON 1.1.0)
+get_package (fmt https://github.com/fmtlib/fmt.git 7.1.3)
+get_package (spdlog https://github.com/gabime/spdlog 1.8.5)
+get_package (rapidJSON https://github.com/Tencent/rapidjson/ 1.1.0)

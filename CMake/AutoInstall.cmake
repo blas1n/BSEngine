7544f9bd6d3083386c4c20b7cf632afa0d416ef8
blAs1N <qazasa123@gmail.com>
2021-04-02 14:41:41 +0900

    Use auto created cmake

--- CMake/AutoInstall.cmake
+++ CMake/AutoInstall.cmake
@@ -1,14 +1,14 @@
 cmake_minimum_required (VERSION 3.10)
-project (${name} NONE)
+project (@NAME@ NONE)
 include (ExternalProject)
 
-ExternalProject_Add (${name}
-	GIT_REPOSITORY		${repo}
-	GIT_TAG				${tag}
+ExternalProject_Add (@NAME@
+	GIT_REPOSITORY		@REPO@
+	GIT_TAG				@TAG@
 	GIT_SHALLOW			TRUE
 	GIT_PROGRESS		TRUE
-	SOURCE_DIR			"${src_dir}"
-	BINARY_DIR			"${build_dir}"
+	SOURCE_DIR			@SRC_DIR@
+	BINARY_DIR			@BUILD_DIR@
 	CONFIGURE_COMMAND	""
 	BUILD_COMMAND		""
 	INSTALL_COMMAND		""

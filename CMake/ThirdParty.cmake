e9a7b6532b879c55a239a1bd23abb740d0d035cd
blAs1N <qazasa123@gmail.com>
2021-04-02 14:43:33 +0900

    Use .git url

--- CMake/ThirdParty.cmake
+++ CMake/ThirdParty.cmake
@@ -2,5 +2,5 @@ find_package (SDL2 REQUIRED)
 find_package (GLEW REQUIRED)
 
 get_package (fmt https://github.com/fmtlib/fmt.git 7.1.3)
-get_package (spdlog https://github.com/gabime/spdlog 1.8.5)
-get_package (rapidJSON https://github.com/Tencent/rapidjson/ 1.1.0)
+get_package (spdlog https://github.com/gabime/spdlog.git v1.8.5)
+get_package (rapidJSON https://github.com/Tencent/rapidjson.git v1.1.0)

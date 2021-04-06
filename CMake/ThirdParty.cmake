498a76fcb78470a361c076f4ed460ce50a255802
blAs1N <qazasa123@gmail.com>
2021-04-06 14:52:51 +0900

    Remove auto install system

--- CMake/ThirdParty.cmake
+++ CMake/ThirdParty.cmake
@@ -1,6 +1,5 @@
 find_package (SDL2 REQUIRED)
 find_package (GLEW REQUIRED)
-
-get_package (fmt https://github.com/fmtlib/fmt.git 7.1.3)
-get_package (spdlog https://github.com/gabime/spdlog.git v1.8.5)
-get_package (rapidJSON https://github.com/Tencent/rapidjson.git v1.1.0)
+find_package (fmt REQUIRED)
+find_package (spdlog REQUIRED)
+find_package (rapidJSON REQUIRED)

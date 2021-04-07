f94673fb57388904e29861b908f0c7c65da6d145
blAs1N <qazasa123@gmail.com>
2021-04-07 14:52:24 +0900

    Add BSBase headers

--- Source/Runtime/Core/Public/Core.h
+++ Source/Runtime/Core/Public/Core.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "BSBase/Base.h"
+#include "BSBase/Result.h"
+#include "BSBase/String.h"
+#include "BSBase/Type.h"
+#include "BSBase/Util.h"
 #include "Assertion.h"
 #include "Log.h"
 #include "Macro.h"

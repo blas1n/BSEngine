96f1af89af16c65a2e33e0056a3a6a2f5f6f4a02
blAs1N <qazasa123@gmail.com>
2021-04-10 17:41:38 +0900

    Optimize delegate

--- Source/Runtime/Core/Public/Delegate.h
+++ Source/Runtime/Core/Public/Delegate.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include "DelegateBase.h"
-#include "DelegateInst.h"
+#include <utility>
 
 template <class T>
 class Delegate
@@ -9,41 +8,93 @@ class Delegate
 	static_assert(sizeof(T) == 0, "Expected a function signature for the delegate template parameter");
 };
 
+// This code was created with reference to https://www.codeproject.com/Articles/1170503/The-Impossibly-Fast-Cplusplus-Delegates-Fixed
 template <class R, class... Args>
-class Delegate<R(Args...)> final : public Impl::DelegateBase
+class Delegate<R(Args...)> final
 {
 public:
-	using Func = R(*)(Args...);
+	Delegate() noexcept
+		: object(nullptr), stub(nullptr) {}
 
-public:
-	using DelegateBase::DelegateBase;
+	Delegate(std::nullptr_t) noexcept : Delegate() {}
+
+	Delegate(const Delegate& other) noexcept
+		: object(other.object), stub(other.stub) {}
+
+	Delegate(Delegate&& other) noexcept
+		: object(std::move(other.object)), stub(std::move(other.stub)) {}
+
+	Delegate& operator=(const Delegate& other) noexcept
+	{
+		object = other.object;
+		stub = other.stub;
+		return *this;
+	}
+
+	Delegate& operator=(Delegate&& other)
+	{
+		object = std::move(other.object);
+		stub = std::move(other.stub);
+		return *this;
+	}
+
 	~Delegate() = default;
 
-	Delegate(Func fn)
-	{
-		Allocate(FunctionDelegateInst{ fn });
-	}
+	template <R(*Function)(Args...)>
+	Delegate(R(*fn)(Args...)) : object(nullptr), stub(&FunctionStub<Function>) {}
 
-	template <class T>
-	Delegate(T&& obj, R(T::*fn)(Args...))
-	{
-		Allocate(MethodDelegateInst{ obj, fn });
-	}
+	template <class T, R(*Method)(Args...)>
+	Delegate(T* obj) : object(obj), stub(&MethodStub<Method>) {}
 
-	template <class Fn>
-	Delegate(Fn&& fn)
-	{
-		Allocate(FunctorDelegateInst<Func>{ fn });
-	}
+	template <class T, R(*Method)(Args...)>
+	Delegate(const T* obj) : object(const_cast<T*>(obj)), stub(&ConstMethodStub<Method>) {}
+
+	template <class Functor>
+	Delegate(Functor&& fn) : object(&fn), stub(&FunctorStub<Functor>) {}
 
 	R operator()(Args&&... args) const
 	{
-		if (const void* ptr = GetPtr())
-		{
-			return reinterpret_cast<DelegateInstBase
-				<Func>*>(ptr)(std::forward<Args>(args)...);
-		}
+		if (!stub) return R();
 
-		return R();
+		return (*stub)(object, std::forward<Args>(args)...);
 	}
+
+	void Clear() noexcept { object = stub = nullptr; }
+
+	[[nodiscard]] bool IsBound() const noexcept { return stub; }
+	[[nodiscard]] operator bool() const noexcept { return stub; }
+
+private:
+	template <R(*Function)(Args...)>
+	static R FunctionStub(void* object, Args... args)
+	{
+		return (Function)(std::forward<Args>(args)...);
+	}
+
+	template <class T, R(T::*Method)(Args...)>
+	static R MethodStub(void* object, Args... args)
+	{
+		T* ptr = static_cast<T*>(object);
+		return (ptr->*Method)(std::forward<Args>(args)...);
+	}
+	
+	template <class T, R(T::*Method)(Args...) const>
+	static R ConstMethodStub(const void* object, Args... args)
+	{
+		const T* ptr = static_cast<const T*>(object);
+		return (ptr->*Method)(std::forward<Args>(args)...);
+	}
+	
+	template <class Functor>
+	static R FunctorStub(void* object, Args... args)
+	{
+		Functor* ptr = static_cast<Functor*>(object);
+		return (ptr->operator())(std::forward<Args>(args)...);
+	}
+
+private:
+	using Stub = R(*)(void*, Args...);
+
+	void* object;
+	Stub stub;
 };

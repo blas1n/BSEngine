4454a2c8b5131a1616b0fc97673521fb6e3944e3
blAs1N <qazasa123@gmail.com>
2021-04-11 21:52:42 +0900

    Create color test

---
+++ Tests/CoreTest.cpp
@@ -0,0 +1,54 @@
+#include "gtest/gtest.h"
+#include "Core.h"
+
+TEST(CoreTest, AssertTest)
+{
+	const int32 lhs = 1, rhs = 1;
+
+	Check(lhs == rhs);
+	CheckMsg(lhs == rhs, u"{} and {} are different.", lhs, rhs);
+
+	EXPECT_TRUE(Ensure(lhs == rhs));
+	EXPECT_TRUE(EnsureMsg(lhs == rhs, u"{} and {} are different.", lhs, rhs));
+}
+
+struct Foo final
+{
+	bool Boo(int32 lhs, int32 rhs)
+	{
+		return true;
+	}
+};
+
+TEST(CoreTest, DelegateTest)
+{
+	auto lambda = [](int32 lhs, int32 rhs)
+	{
+		return lhs == rhs;
+	};
+
+	Foo foo;
+	Delegate<bool, int32, int32> delegate{ &foo, &Foo::Boo };
+
+	EXPECT_TRUE(delegate(1, 1));
+}
+
+static bool ReturnTrue(int32 lhs, int32 rhs)
+{
+	return true;
+}
+
+TEST(CoreTest, EventTest)
+{
+	Event<bool, int32, int32> event;
+	event += [](int32 lhs, int32 rhs)
+	{
+		return lhs == rhs;
+	};
+
+	event(1, 1);
+	/*EXPECT_TRUE(event([](bool result, bool newResult)
+		{
+			return result && newResult;
+		}, 1, 1));*/
+}

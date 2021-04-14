2a18ddba8efcc02a8cf34dd90420c45932a2af9c
blAs1N <qazasa123@gmail.com>
2021-04-14 22:15:23 +0900

    Auto install vcpkg

--- Scripts/Setup.sh
+++ Scripts/Setup.sh
@@ -2,6 +2,31 @@
 
 set -e
 
+if [ -d "vcpkg" ]; then
+    cd vcpkg
+else
+    git clone https://github.com/Microsoft/vcpkg.git
+    cd vcpkg
+    sh bootstrap-vcpkg.sh
+fi
+
+CHECK_OS="`uname -s`"
+
+if [[ "$CHECK_OS" = "Darwin"* ]]; then
+    VCPKG_DEFAULT_TRIPLET=x64-osx
+elif [[ "$CHECK_OS" = "Linux"* ]]; then
+    VCPKG_DEFAULT_TRIPLET=x64-linux
+elif [[ "$CHECK_OS" = "MINGW64"* ]]; then
+    VCPKG_DEFAULT_TRIPLET=x64-windows
+elif [[ "$CHECK_OS" = "CYGWIN"* ]]; then
+    VCPKG_DEFAULT_TRIPLET=x64-windows
+else
+    echo "Unknown OS!"
+    exit 1
+fi
+
+VCPKG_DEFAULT_TRIPLET=x64-windows
+
 vcpkg install fmt
 vcpkg install spdlog
 vcpkg install rapidJSON

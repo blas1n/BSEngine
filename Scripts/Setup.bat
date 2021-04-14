9562872df7e1702ce98959738f3e0d1c156d9c13
blAs1N <qazasa123@gmail.com>
2021-04-14 22:15:23 +0900

    Auto install vcpkg

--- Scripts/Setup.bat
+++ Scripts/Setup.bat
@@ -1,5 +1,16 @@
 @echo off
 
+if exist vcpkg\ (
+    cd vcpkg
+    
+) else (
+    git clone https://github.com/Microsoft/vcpkg.git
+    cd vcpkg
+    ./bootstrap-vcpkg
+)
+
+set VCPKG_DEFAULT_TRIPLET=x64-windows
+
 vcpkg install fmt
 vcpkg install spdlog
 vcpkg install rapidJSON

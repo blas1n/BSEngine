848488ef015082906deee7e97907294c657f85bc
blAs1N <qazasa123@gmail.com>
2021-04-15 21:13:39 +0900

    Clear cmake and script

--- Scripts/Setup.bat
+++ Scripts/Setup.bat
@@ -1,25 +1,8 @@
 @echo off
 
-cd ../ThirdParty
-
-if exist vcpkg\ (
-    cd vcpkg
-) else (
-    git clone https://github.com/Microsoft/vcpkg.git
-    cd vcpkg
-
-    setx VCPKG_ROOT %CD%\vcpkg
-    bootstrap-vcpkg
-)
-
-set VCPKG_DEFAULT_TRIPLET=x64-windows
-
 vcpkg install SDL2
 vcpkg install glew
 vcpkg install fmt
 vcpkg install spdlog
 vcpkg install rapidJSON
 vcpkg install utfcpp
-vcpkg integrate install
-
-cd ../../Scripts

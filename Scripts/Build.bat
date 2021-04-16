210f9aa2e2400d2fc2db49967d10b2b9a248e4f2
blAs1N <qazasa123@gmail.com>
2021-04-16 14:54:55 +0900

    Set script's path (only windows)

--- Scripts/Build.bat
+++ Scripts/Build.bat
@@ -1,3 +1,5 @@
 @echo off
 
+pushd %~dp0
 cmake --build ../Binaries
+popd
--- Scripts/Setup.bat
+++ Scripts/Setup.bat
@@ -1,3 +1,5 @@
 @echo off
 
+pushd %~dp0
 cmake -S .. -B ../Binaries/ -DCMAKE_TOOLCHAIN_FILE=../ThirdParty/vcpkg/scripts/buildsystems/vcpkg.cmake
+popd

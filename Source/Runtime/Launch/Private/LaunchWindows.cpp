c21e2e6271697f9d698fa22c3d70abc11c316f93
blAs1N <qazasa123@gmail.com>
2021-04-21 17:00:26 +0900

    Fix windows.h including error

--- Source/Runtime/Launch/Private/LaunchWindows.cpp
+++ Source/Runtime/Launch/Private/LaunchWindows.cpp
@@ -1,13 +1,17 @@
 #define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
 
-#include <windows.h>
 #include "Core.h"
+
+// @note: Error if you include windows.h first
+#include <windows.h>
 
 extern int32 GuardedMain(StringView cmdLine);
 
 String ProcessCommandLine()
 {
-	return CastCharSet<Char>(std::wstring_view{ GetCommandLineW() });
+	const auto cmd = std::wstring{ GetCommandLineW() };
+	return CastCharSet<Char>(std::wstring_view{ cmd.c_str() });
 }
 
 int32 WINAPI WinMain(_In_ HINSTANCE hInInstance, _In_opt_ HINSTANCE, _In_ char*, _In_ int32)

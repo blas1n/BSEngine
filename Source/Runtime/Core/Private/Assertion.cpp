4dbc2462d2cb5418522948f06d45b518684beda0
blAs1N <qazasa123@gmail.com>
2021-04-22 14:17:31 +0900

    Use String View instead of Log Category

--- Source/Runtime/Core/Private/Assertion.cpp
+++ Source/Runtime/Core/Private/Assertion.cpp
@@ -3,11 +3,9 @@
 
 #ifndef NDEBUG
 
-DEFINE_LOG_CATEGORY(LogAssert)
-
 void Impl::LogToFail(bool isCritical, const Char* expr, const Char* file, BSBase::int32 line, const String& msg) noexcept
 {
-	Log(LogAssert, isCritical ? LogVerbosity::Critical : LogVerbosity::Error,
+	Log(STR(""), isCritical ? LogVerbosity::Critical : LogVerbosity::Error,
 		STR("{} failed: {} {}, file: {}, line: {}"),
 		isCritical ? STR("Check") : STR("Ensure"), expr, msg, file, line);
 }

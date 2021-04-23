d992d982a242018417bcf1e45c6698b7bc0c33ea
blAs1N <qazasa123@gmail.com>
2021-04-23 14:51:09 +0900

    Regenerate assert

--- Source/Runtime/Core/Private/Assertion.cpp
+++ Source/Runtime/Core/Private/Assertion.cpp
@@ -1,13 +1,15 @@
 #include "Assertion.h"
-#include "Log.h"
+#include "Logger.h"
 
 #ifndef NDEBUG
 
-void Impl::LogToFail(bool isCritical, const Char* expr, const Char* file, BSBase::int32 line, const String& msg) noexcept
+void Impl::LogToFail(Logger& logger, bool isCritical, const Char* expr, const Char* file, BSBase::int32 line, const String& msg) noexcept
 {
-	Log(STR(""), isCritical ? LogVerbosity::Critical : LogVerbosity::Error,
+	logger.Log(
+		isCritical ? LogVerbosity::Critical : LogVerbosity::Error,
 		STR("{} failed: {} {}, file: {}, line: {}"),
-		isCritical ? STR("Check") : STR("Ensure"), expr, msg, file, line);
+		isCritical ? STR("Assert") : STR("Ensure"), expr, msg, file, line
+	);
 }
 
 #endif
--- Source/Runtime/Core/Public/Assertion.h
+++ Source/Runtime/Core/Public/Assertion.h
@@ -3,45 +3,52 @@
 #include "BSBase/Type.h"
 #include "fmt/format.h"
 #include "CharSet.h"
-#include "LogCategory.h"
 #include "Platform.h"
 
 #ifdef NDEBUG
 
-#	define CheckMsg(expr, fmt, ...) (void)(expr)
-#	define Check(expr) (void)(expr)
+#	define Assert(expr)
+#	define AssertLog(expr, logger)
+#	define AssertMsg(expr, logger, fmt, ...)
 
-#	define EnsureMsg(expr, fmt, ...) !!(expr)
-#	define Ensure(expr) !!(expr)
+#	define Ensure(expr)
+#	define EnsureLog(expr, logger)
+#	define EnsureMsg(expr, logger, fmt, ...)
 
 #else
 
+class Logger;
+
 namespace Impl
 {
-	CORE_API void LogToFail(bool isCritical, const Char* expr, const Char* file, BSBase::int32 line, const String& msg) noexcept;
+	CORE_API void LogToFail(Logger& logger, bool isCritical, const Char* expr, const Char* file, BSBase::int32 line, const String& msg) noexcept;
 
 	template <class Str, class... Args>
-	void LogToFail(bool isCritical, const Char* expr, const Char* file, BSBase::int32 line, const Str& format, Args... args) noexcept
+	void LogToFail(Logger& logger, bool isCritical, const Char* expr, const Char* file, BSBase::int32 line, const Str& format, Args... args) noexcept
 	{
-		LogToFail(isCritical, expr, file, line, fmt::format(format, args...));
+		LogToFail(logger, isCritical, expr, file, line, fmt::format(format, args...));
 	}
 }
 
-#	ifdef _WIN32
+#	ifdef PLATFORM_WINDOWS
 #		define DEBUG_BREAK() (void)(IsDebugging() && (::__debugbreak(), true))
 #	else
 #		include <csignal>
 #		define DEBUG_BREAK() (void)(IsDebugging() && (::std::raise(SIGTRAP), true))
 #	endif
 
-#	define CheckMsg(expr, fmt, ...) (void)(!!(expr) || (Impl::LogToFail(true, \
+#	define Assert(expr) (void)(!!(expr) || (DEBUG_BREAK(), false))
+
+#	define AssertMsg(expr, logger, fmt, ...) (void)(!!(expr) || (Impl::LogToFail(true, \
 		u#expr, ADD_PREFIX(u, __FILE__), __LINE__, fmt, ##__VA_ARGS__), DEBUG_BREAK(), false))
 
-#	define Check(expr) CheckMsg(expr, STR(""))
+#	define AssertLog(expr, logger) AssertMsg(expr, logger, STR(""))
 
-#	define EnsureMsg(expr, fmt, ...) (!!(expr) || (Impl::LogToFail(false, \
+#	define Ensure(expr) (!!(expr) || (DEBUG_BREAK(), false))
+
+#	define EnsureMsg(expr, logger, fmt, ...) (!!(expr) || (Impl::LogToFail(false, \
 		u#expr, ADD_PREFIX(u, __FILE__), __LINE__, fmt, ##__VA_ARGS__), DEBUG_BREAK(), false))
 
-#	define Ensure(expr) EnsureMsg(expr, STR(""))
+#	define EnsureLog(expr, logger) EnsureMsg(expr, logger, STR(""))
 
 #endif

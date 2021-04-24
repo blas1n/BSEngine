ec5405e01cdb573c6c916a56c496f2c6a739d685
blAs1N <qazasa123@gmail.com>
2021-04-24 22:51:49 +0900

    Use static local variable in assertion

--- Source/Runtime/Core/Private/Assertion.cpp
+++ Source/Runtime/Core/Private/Assertion.cpp
@@ -3,13 +3,37 @@
 
 #ifndef NDEBUG
 
-void Impl::LogToFail(Logger& logger, bool isCritical, const Char* expr, const Char* file, BSBase::int32 line, const String& msg) noexcept
+void Impl::LogToFail(bool isCritical, const Char* expr, const Char* file, BSBase::int32 line, const String& msg) noexcept
 {
-	logger.Log(
-		isCritical ? LogVerbosity::Critical : LogVerbosity::Error,
-		STR("{} failed: {} {}, file: {}, line: {}"),
-		isCritical ? STR("Assert") : STR("Ensure"), expr, msg, file, line
-	);
+	static Logger assertLogger{ STR("Assert") };
+	static Logger ensureLogger{ STR("Ensure") };
+	static bool bInit = false;
+
+	if (!bInit)
+	{
+		assertLogger.AddSink<Sink::FileSink>(STR("Assert.log"));
+		ensureLogger.AddSink<Sink::FileSink>(STR("Ensure.log"));
+
+		assertLogger.AddSink<Sink::StderrSink>();
+		ensureLogger.AddSink<Sink::StderrSink>();
+		
+		bInit = true;
+	}
+
+	if (isCritical)
+	{
+		assertLogger.Log(LogVerbosity::Critical,
+			STR("{} failed: {} {}, file: {}, line: {}"),
+			isCritical ? STR("Assert") : STR("Ensure"), expr, msg, file, line
+		);
+	}
+	else
+	{
+		ensureLogger.Log(LogVerbosity::Error,
+			STR("{} failed: {} {}, file: {}, line: {}"),
+			isCritical ? STR("Assert") : STR("Ensure"), expr, msg, file, line
+		);
+	}
 }
 
 #endif

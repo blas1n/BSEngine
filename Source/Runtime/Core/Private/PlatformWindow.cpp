d26f3c92441f236cd4b8d06a2ce44c19b1f1dec2
blAs1N <qazasa123@gmail.com>
2021-04-24 23:06:16 +0900

    Use string view instead of string in dll

--- Source/Runtime/Core/Private/PlatformWindow.cpp
+++ Source/Runtime/Core/Private/PlatformWindow.cpp
@@ -39,8 +39,8 @@ namespace
     }
 }
 
-Dll::Dll(const String& inPath)
-    : path(inPath)
+Dll::Dll(StringView inPath)
+    : path(inPath.data())
 {
     const std::wstring wPath(path.cbegin(), path.cend());
     dll = LoadLibraryW(wPath.c_str());
@@ -86,7 +86,7 @@ Dll::~Dll()
     FreeLibrary(reinterpret_cast<HMODULE>(dll));
 }
 
-void* Dll::GetSymbol(const String& name) const
+void* Dll::GetSymbol(StringView name) const
 {
     const auto symbol = FindSymbol(name);
     if (EnsureMsg(symbol, STR("Path: {}, Name: {}, {}"), path, name, GetLastErrorMsg()))
@@ -95,7 +95,7 @@ void* Dll::GetSymbol(const String& name) const
     return nullptr;
 }
 
-void* Dll::FindSymbol(const String& name) const noexcept
+void* Dll::FindSymbol(StringView name) const noexcept
 {
     const auto buf = CastCharSet<char>(StringView{ name });
     return reinterpret_cast<void*>(GetProcAddress(reinterpret_cast<HMODULE>(dll), buf.c_str()));

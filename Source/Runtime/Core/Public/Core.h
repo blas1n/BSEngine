43fc456a886d3fe19befe2a52fb11c5c176789d7
blAs1N <qazasa123@gmail.com>
2021-04-25 20:06:47 +0900

    Fix wrong include order

--- Source/Runtime/Core/Public/Core.h
+++ Source/Runtime/Core/Public/Core.h
@@ -1,17 +1,15 @@
 #pragma once
 
-#include "BSBase/Base.h"
 #include "BSBase/Result.h"
-#include "BSBase/Type.h"
 #include "BSBase/Util.h"
 #include "Assertion.h"
+#include "BSMath.h"
 #include "CharSet.h"
 #include "ConfigFile.h"
 #include "Delegate.h"
 #include "Event.h"
 #include "Json.h"
 #include "Logger.h"
-#include "BSMath.h"
 #include "Platform.h"
 
 using namespace BSBase;

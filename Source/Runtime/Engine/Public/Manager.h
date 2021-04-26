676a5c22e8cfb72d978aeb54891662d9b438603d
blAs1N <qazasa123@gmail.com>
2021-04-26 14:24:25 +0900

    Create manager interface

---
+++ Source/Runtime/Engine/Public/Manager.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Core.h"
+
+class ENGINE_API Manager
+{
+public:
+	Manager(class Engine* inEngine)
+		: engine(inEngine) {}
+
+	Manager(const Manager&) = delete;
+	Manager(Manager&&) noexcept = delete;
+
+	Manager& operator=(const Manager&) = delete;
+	Manager& operator=(Manager&&) noexcept = delete;
+
+	virtual ~Manager() {}
+
+	[[nodiscard]] virtual int32 Init() noexcept {}
+	virtual void Update() noexcept {}
+	virtual void Release() noexcept {}
+
+protected:
+	Engine* GetEngine() noexcept { return engine; }
+	const Engine* GetEngine() const noexcept { return engine; }
+
+private:
+	Engine* engine;
+};

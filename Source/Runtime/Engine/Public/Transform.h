1f301f98cff5ea7ad6c71b04ddc8d258957175c5
blAs1N <qazasa123@gmail.com>
2021-05-04 19:14:05 +0900

    Implement transform

--- Source/Runtime/Engine/Public/Transform.h
+++ Source/Runtime/Engine/Public/Transform.h
@@ -4,50 +4,43 @@
 #include "BSMath.h"
 #include <vector>
 
-namespace Impl
-{
-	struct Node final
-	{
-		Transform* ptr;
-		uint32 id;
-	};
-}
-
 class ENGINE_API Transform final : public Component
 {
 public:
 	using Super = Component;
 	using Super::Super;
 
-	Matrix4 GetWorldMatrix() const;
-
-	void SetParent(uint32 inParent);
-	void AddChild(uint32 inChild);
+	[[nodiscard]] Matrix4 GetWorldMatrix();
 
 	void Serialize(Json& json) override;
 	void Deserialize(const Json& json) override;
 
-	const Vector3& GetPosition() const noexcept { return position; }
-	const Rotator& GetRotation() const noexcept { return rotation; }
-	const Vector3& GetScale() const noexcept { return scale; }
+	[[nodiscard]] uint32 GetParent() const noexcept { return parent; }
+	void SetParent(uint32 inParent);
+
+	[[nodiscard]] const Vector3& GetPosition() const noexcept { return position; }
+	[[nodiscard]] const Rotator& GetRotation() const noexcept { return rotation; }
+	[[nodiscard]] const Vector3& GetScale() const noexcept { return scale; }
 
 	void SetPosition(const Vector3& inPos) noexcept { position = inPos; isMatUpdated = false; }
 	void SetRotation(const Rotator& inRot) noexcept { rotation = inRot; isMatUpdated = false; }
 	void SetScale(const Vector3& inScale) noexcept { scale = inScale; isMatUpdated = false; }
 
 private:
-	void SetParentImpl(Impl::Node inParent) { parent = inParent; }
-	void AddChildImpl(Impl::Node inParent) { children.push_back(inParent); }
+	void SetParentTransform();
+	void OnChangeParentId(uint32 newId, uint32 oldId) { parent = newId; }
 
 private:
-	mutable Matrix4 mat;
+	constexpr static uint32 ParentNone = static_cast<uint32>(-1);
+
+	Matrix4 mat;
 
 	Vector3 position;
 	Rotator rotation;
 	Vector3 scale;
 
-	std::vector<Impl::Node> children;
-	Impl::Node parent;
+	Transform* parentTransform;
+	uint32 parent = ParentNone;
 
-	mutable uint8 isMatUpdated : 1;
+	uint8 isMatUpdated : 1;
 };

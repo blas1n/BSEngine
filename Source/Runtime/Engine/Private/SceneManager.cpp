608c5005726ee125c48d2dd3ede7a4b15ab3b443
blAs1N <qazasa123@gmail.com>
2021-05-05 18:11:22 +0900

    Load async in scene

--- Source/Runtime/Engine/Private/SceneManager.cpp
+++ Source/Runtime/Engine/Private/SceneManager.cpp
@@ -1,4 +1,5 @@
 #include "SceneManager.h"
+#include "ThreadManager.h"
 
 bool SceneManager::Update(float deltaTime) noexcept
 {
@@ -12,14 +13,36 @@ bool SceneManager::Update(float deltaTime) noexcept
 	return !isEnd;
 }
 
-bool SceneManager::Load(Name name) noexcept
+std::future<bool> SceneManager::Load(Name name) noexcept
 {
+	Delegate<bool(void)> load{ [this, name] { return LoadImpl(name); } };
+	return Accessor<ThreadManager>::GetManager()->AddTask(std::move(load));
+}
+
+void SceneManager::RegisterUpdate(const Delegate<void(float)>& callback)
+{
+	std::shared_lock lock{ mutex };
+	onUpdates[isFrontScene != isLoadScene] += callback;
+}
+
+void SceneManager::RegisterUpdate(Delegate<void(float)>&& callback)
+{
+	std::shared_lock lock{ mutex };
+	onUpdates[isFrontScene != isLoadScene] += std::move(callback);
+}
+
+bool SceneManager::LoadImpl(Name name) noexcept
+{
+	mutex.lock();
 	isLoadScene = true;
-	
+	mutex.unlock();
+
 	const bool isSuccess = scenes[!isFrontScene].Load(name);
 
+	mutex.lock();
 	isLoadScene = false;
 	isSwapScene = true;
+	mutex.unlock();
 
 	return isSuccess;
 }

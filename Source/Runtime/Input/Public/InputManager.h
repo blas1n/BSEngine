1045390db33348a00668dcb8344ca59f28ea3d58
blAs1N <qazasa123@gmail.com>
2021-05-06 02:58:48 +0900

    Create simple input module

--- Source/Runtime/Input/Public/InputManager.h
+++ Source/Runtime/Input/Public/InputManager.h
@@ -1,18 +1,32 @@
 #pragma once
 
 #include "Core.h"
+#include "Accessor.h"
 #include "Manager.h"
 
-class INPUT_API InputManager : public Manager
+class INPUT_API InputManager : public Manager, private Accessor<class WindowManager>
 {
 public:
 	[[nodiscard]] bool Init() noexcept override;
 	[[nodiscard]] bool Update(float deltaTime) noexcept override;
 	void Release() noexcept override;
 
-private:
-	struct Impl* impl;
+	bool IsPressed(uint8 key) const noexcept { return keyStates[isFrontState][key]; }
+	bool IsReleased(uint8 key) const noexcept { return !keyStates[isFrontState][key]; }
+	bool IsPress(uint8 key) const noexcept { return keyStates[isFrontState][key] && !keyStates[!isFrontState][key]; }
+	bool IsRelease(uint8 key) const noexcept { return !keyStates[isFrontState][key] && keyStates[!isFrontState][key]; }
 
-	uint8 keyState[256];
+	const IntVector2& GetMousePos() const noexcept { return mousePos; }
+
+private:
+	bool ReadKeyboard();
+	bool ReadMouse();
+
+private:
+	struct InputImpl* impl;
+
+	uint8 keyStates[2][256];
 	IntVector2 mousePos;
+
+	uint8 isFrontState : 1;
 };

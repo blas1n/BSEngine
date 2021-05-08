c1b8690f495669b843346eec65065f1b1add286d
blAs1N <qazasa123@gmail.com>
2021-05-08 23:42:56 +0900

    Create input action and input axis

--- Source/Runtime/Input/Public/InputManager.h
+++ Source/Runtime/Input/Public/InputManager.h
@@ -3,7 +3,44 @@
 #include "Core.h"
 #include "Accessor.h"
 #include "Manager.h"
+#include <variant>
 #include "InputCode.h"
+
+using InputCode = std::variant<KeyCode, MouseCode, MouseAxis>;
+
+struct INPUT_API InputAction final
+{
+	constexpr InputAction(KeyCode inCode, KeyMode inMode = KeyMode::None) noexcept
+		: code(inCode), mode(inMode) {}
+
+	constexpr InputAction(MouseCode inCode, KeyMode inMode = KeyMode::None) noexcept
+		: code(inCode), mode(inMode) {}
+
+	constexpr InputAction(MouseAxis inAxis, KeyMode inMode = KeyMode::None) noexcept
+		: code(inAxis), mode(inMode) {}
+
+	explicit InputAction(const Json& json);
+
+	InputCode code;
+	KeyMode mode;
+};
+
+struct INPUT_API InputAxis final
+{
+	constexpr InputAxis(KeyCode inCode, float inScale = 1.0f) noexcept
+		: code(inCode), scale(inScale) {}
+	
+	constexpr InputAxis(MouseCode inCode, float inScale = 1.0f) noexcept
+		: code(inCode), scale(inScale) {}
+	
+	constexpr InputAxis(MouseAxis inAxis, float inScale = 1.0f) noexcept
+		: code(inAxis), scale(inScale) {}
+
+	explicit InputAxis(const Json& json);
+
+	InputCode code;
+	float scale;
+};
 
 class INPUT_API InputManager : public Manager, private Accessor<class WindowManager>
 {

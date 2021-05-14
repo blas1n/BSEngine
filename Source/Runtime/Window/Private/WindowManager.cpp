423a903d1dadc0241a6f41e926107efddfa4de0c
blAs1N <qazasa123@gmail.com>
2021-05-14 15:38:09 +0900

    Remove temp background

--- Source/Runtime/Window/Private/WindowManager.cpp
+++ Source/Runtime/Window/Private/WindowManager.cpp
@@ -39,7 +39,7 @@ bool WindowManager::Init() noexcept
     wc.hInstance = hInstance;
     wc.hIcon = wc.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
     wc.hCursor = nullptr;
-    wc.hbrBackground = reinterpret_cast<HBRUSH>(GetStockObject(WHITE_BRUSH)); // Temp background
+    wc.hbrBackground = nullptr;
     wc.lpszMenuName = nullptr;
     wc.lpszClassName = ClassName;
     wc.cbSize = sizeof(WNDCLASSEX);

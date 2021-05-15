20e3f9ed374ae9ffb5afbb57bd1c55b602c5457e
blAs1N <qazasa123@gmail.com>
2021-05-15 18:57:01 +0900

    Apply onChangedName

--- Source/Runtime/Engine/Public/Scene.h
+++ Source/Runtime/Engine/Public/Scene.h
@@ -23,13 +23,19 @@ public:
 	Entity* AddEntity(const String& name);
 	Entity* AddEntity(const String& name, Entity* prefab);
 
-	bool RemoveEntity(Entity* entity);
-	bool RemoveEntity(const String& name) { return entities.erase(name); }
+	bool RemoveEntity(const String& name);
+	bool RemoveEntity(Entity* entity) { return RemoveEntity(entity->GetName()); }
 
 	[[nodiscard]] Entity* GetEntity(const String& name) noexcept { return &entities.at(name); }
 	[[nodiscard]] const Entity* GetEntity(const String& name) const noexcept { return &entities.at(name); }
 	
 	[[nodiscard]] Name GetName() const noexcept { return name; }
+
+private:
+	Entity* AddEntityImpl(Entity& entity);
+	void RemoveEntityImpl(Entity& entity);
+
+	void OnChangedName(Entity& entity, const String& after, const String& before);
 
 private:
 	std::unordered_map<String, Entity> entities;
